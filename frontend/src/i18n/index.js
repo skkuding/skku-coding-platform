e012bd1daba3f085c53920589ed3d4c42a42f2eb
Byeonghyeon Lee <dlqudgus2587@gmail.com>
2021-02-02 11:08:46 +0900

    유저 선호 언어 설정 구현

--- frontend/src/i18n/index.js
+++ frontend/src/i18n/index.js
@@ -2,26 +2,20 @@ import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 // ivew UI
 import ivenUS from 'iview/dist/locale/en-US'
-import ivzhCN from 'iview/dist/locale/zh-CN'
-import ivzhTW from 'iview/dist/locale/zh-TW'
 // element UI
 import elenUS from 'element-ui/lib/locale/lang/en'
-import elzhCN from 'element-ui/lib/locale/lang/zh-CN'
-import elzhTW from 'element-ui/lib/locale/lang/zh-TW'
 
 Vue.use(VueI18n)
 
 const languages = [
-  { value: 'en-US', label: 'English', iv: ivenUS, el: elenUS },
-  { value: 'zh-CN', label: '简体中文', iv: ivzhCN, el: elzhCN },
-  { value: 'zh-TW', label: '繁體中文', iv: ivzhTW, el: elzhTW }
+  { value: 'en-US', label: 'English', iv: ivenUS, el: elenUS }
 ]
 const messages = {}
 
-// combine admin and oj
-for (const lang of languages) {
-  const locale = lang.value
-  const m = require(`./oj/${locale}`).m
+// combine admin and ojs
+for (let lang of languages) {
+  let locale = lang.value
+  let m = require(`./oj/${locale}`).m
   Object.assign(m, require(`./admin/${locale}`).m)
   const ui = Object.assign(lang.iv, lang.el)
   messages[locale] = Object.assign({ m: m }, ui)
