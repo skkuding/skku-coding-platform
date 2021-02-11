2e1fcf26da47377f0654ea886f5a882dfff43e47
Jaemin Choi <1dotolee@gmail.com>
2021-02-12 01:38:12 +0900

    Implement header with bootstrap-vue
    - 기본 헤더 구현 (route 추가 필요)
    - Manrope 글꼴 추가
    - scss 디자인 팀 구현 사항 추가
    - 학교 로고 이미지(png, svg) 추가
    
    Co-authored-by: JooyoungKim <illuminoplanet@gmail.com>

--- frontend/src/pages/oj/components/Header.vue
+++ frontend/src/pages/oj/components/Header.vue
@@ -1,20 +1,20 @@
 <template>
   <div id="header">
-    <b-navbar>
-      <b-navbar-brand class="header__logo">
+    <b-navbar style="height: 100%">
+      <b-navbar-brand class="ml-5">
         <img
-          src="https://www.skku.edu/_res/skku//img/common/logo.png"
-          style="margin-left: 20px"
+          src="../../../assets/signature.png"
+          style="height: 50px; width: auto;"
         />
       </b-navbar-brand>
 
-      <b-navbar-nav class="header__items" align="center">
+      <b-navbar-nav class="mx-auto">
         <b-nav-item>Notice</b-nav-item>
         <b-nav-item>Contests</b-nav-item>
         <b-nav-item>Problems</b-nav-item>
       </b-navbar-nav>
 
-      <b-navbar-nav class="header__account" align="right">
+      <b-navbar-nav class="mr-5">
         <b-icon icon="person" style="width: 32px; height: 32px"></b-icon>
       </b-navbar-nav>
     </b-navbar>
@@ -22,65 +22,62 @@
 </template>
 
 <script>
-//   import { mapGetters, mapActions } from 'vuex'
-// import login from '@oj/views/user/Login'
-// import register from '@oj/views/user/Register'
-//   export default {
-// components: {
-//   login,
-//   register
-// },
-// mounted () {
-//   this.getProfile()
-// },
-// methods: {
-//   ...mapActions(['getProfile', 'changeModalStatus']),
-//   handleRoute (route) {
-//     if (route && route.indexOf('admin') < 0) {
-//       this.$router.push(route)
-//     } else {
-//       window.open('/admin/')
-//     }
-//   },
-//   handleBtnClick (mode) {
-//     if (mode === 'register') {
-//       this.$alert('아이디(Username)는 자신의 학번으로 설정해주세요')
-//     }
-//     this.changeModalStatus({
-//       visible: true,
-//       mode: mode
-//     })
-//   }
-// },
-// computed: {
-//   ...mapGetters(['website', 'modalStatus', 'user', 'isAuthenticated', 'isAdminRole']),
-//   // Follow routing changes
-//   activeMenu () {
-//     return '/' + this.$route.path.split('/')[1]
-//   },
-//   modalVisible: {
-//     get () {
-//       return this.modalStatus.visible
-//     },
-//     set (value) {
-//       this.changeModalStatus({visible: value})
-//     }
-//   }
-// }
-//   }
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  mounted () {
+    this.getProfile()
+  },
+  methods: {
+    ...mapActions(['getProfile', 'changeModalStatus']),
+    handleRoute (route) {
+      if (route && route.indexOf('admin') < 0) {
+        this.$router.push(route)
+      } else {
+        window.open('/admin/')
+      }
+    },
+    handleBtnClick (mode) {
+      if (mode === 'register') {
+        this.$alert('아이디(Username)는 자신의 학번으로 설정해주세요')
+      }
+      this.changeModalStatus({
+        visible: true,
+        mode: mode
+      })
+    }
+  },
+  computed: {
+    ...mapGetters(['website', 'modalStatus', 'user', 'isAuthenticated', 'isAdminRole']),
+    // 跟随路由变化
+    activeMenu () {
+      return '/' + this.$route.path.split('/')[1]
+    },
+    modalVisible: {
+      get () {
+        return this.modalStatus.visible
+      },
+      set (value) {
+        this.changeModalStatus({ visible: value })
+      }
+    }
+  }
+}
 </script>
 
-<style lang="less" scoped>
-  #header {
-    position: fixed;
-    top: 0;
-    left: 0;
-    height: 72px;
-    width: 100%;
-    z-index: 1000;
-    background-color: #fff;
-  }
-  .header__items {
-    width: 100%;
-  }
+<style scoped>
+#header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 72px;
+  width: 100%;
+  z-index: 1000;
+  background-color: #fff;
+}
+
+.navbar-center {
+  position: absolute;
+  left: 50%;
+}
 </style>
