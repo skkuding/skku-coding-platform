f0770133698ad7ca29f97ccbba8381d61fb88bb3
SOLBI1028 <75013515+SOLBI1028@users.noreply.github.com>
2021-02-16 20:57:39 +0900

    2nd commit

--- frontend/src/pages/oj/views/contest/ContestList.vue
+++ frontend/src/pages/oj/views/contest/ContestList.vue
@@ -8,11 +8,20 @@
       <div class="table">
         <b-table
           hover
-          :items="items"
+          :items="contests"
+          :fields="contestListColumns"
           :per-page="perPage"
           :current-page="currentPage"
           head-variant="light"
-        ></b-table>
+          @row-clicked="goContest"
+        >
+          <!-- <template #cell(title)="data">
+            <a href="#">{{data.value}}</a>
+          </template> -->
+          <!-- <template #cell(status)>
+            {{countdown}}
+          </template> -->
+        </b-table>
       </div>
       <div class="pagination">
           <b-pagination
@@ -28,23 +37,162 @@
 </template>
 
 <script>
+// import moment from 'moment'
+import api from '@oj/api'
+import { mapState, mapGetters, mapActions } from 'vuex'
+import utils from '@/utils/utils'
+import time from '@/utils/time'
+import { CONTEST_STATUS_REVERSE, CONTEST_TYPE, CONTEST_STATUS } from '@/utils/constants'
+
 export default {
+  name: 'ContestList',
+  beforeRouteEnter (to, from, next) {
+    api.getContestList(0, 5).then((res) => {
+      next((vm) => {
+        vm.contests = res.data.data.results
+        vm.total = res.data.data.total
+      })
+    }, (res) => {
+      next()
+    })
+  },
   data () {
     return {
-      perPage: 10,
-      currentPage: 1,
-      items: [
-        { title: 'SKKU Coding Platform 2차 모의대회', start_time: '2021-12-31 15:00', duration: '02:00', status: 'Ongoing' },
-        { title: 'SKKU Coding Platform 3차 모의대회', start_time: '2021-01-01 15:30', duration: '02:15', status: 'Before Start 29:02:07' },
-        { title: 'SKKU Coding Platform 모의대회', start_time: '2021-02-20 15:00', duration: '03:00', status: 'Ended' }
+      CONTEST_STATUS: CONTEST_STATUS,
+      route_name: '',
+      contestID: '',
+      page: 1,
+      query: {
+        status: '',
+        keyword: '',
+        rule_type: ''
+      },
+      // limit: limit,
+      total: 0,
+      rows: '',
+      contests: [],
+      CONTEST_STATUS_REVERSE: CONTEST_STATUS_REVERSE,
+      //      for password modal use
+      cur_contest_id: '',
+      contestListColumns: [
+        {
+          label: 'Title',
+          key: 'title'
+          // formatter: 'goContestDetails'
+          // render: (h, params) => {
+          //   return h('Button', {
+          //     props: {
+          //       type: 'text',
+          //       size: 'large'
+          //     },
+          //     on: {
+          //       click: () => {
+          //         this.$router.push({ name: 'contest-details', params: { contestID: params.row._id } })
+          //       }
+          //     }
+          //   }, params.row.title)
+          // }
+        },
+        {
+          label: 'Start time',
+          key: 'start_time',
+          formatter: 'getFormat'
+          // formatter: value => {
+          //   return time.utcToLocal(value, 'YYYY-M-D HH:mm')
+          // }
+        },
+        {
+          label: 'Duration',
+          key: 'duration'
+          // formatter: value => {
+          //   return this.getDuration(value.row.start_time, value.row.end_time)
+          // }
+        },
+        {
+          label: 'Status',
+          key: 'status'
+        }
       ]
     }
   },
+  methods: {
+    ...mapActions(['changeDomTitle']),
+    handleRoute (route) {
+      this.$router.push(route)
+    },
+    init () {
+      const route = this.$route.query
+      this.query.status = route.status || ''
+      this.query.rule_type = route.rule_type || ''
+      this.query.keyword = route.keyword || ''
+      this.page = parseInt(route.page) || 1
+      this.getContestList()
+    },
+    getContestList (page = 1) {
+      const offset = (page - 1) * this.limit
+      api.getContestList(offset, this.limit, this.query).then((res) => {
+        this.contests = res.data.data.results
+        this.total = res.data.data.total
+      })
+    },
+    changeRoute () {
+      const query = Object.assign({}, this.query)
+      query.page = this.page
+      this.$router.push({
+        name: 'contest-list',
+        query: utils.filterEmptyValue(query)
+      })
+    },
+    onRuleChange (rule) {
+      this.query.rule_type = rule
+      this.page = 1
+      this.changeRoute()
+    },
+    onStatusChange (status) {
+      this.query.status = status
+      this.page = 1
+      this.changeRoute()
+    },
+    goContest (item) {
+      this.cur_contest_id = item.id
+      if (item.contest_type !== CONTEST_TYPE.PUBLIC && !this.isAuthenticated) {
+        this.$error(this.$i18n.t('m.Please_login_first'))
+        this.$store.dispatch('changeModalStatus', { visible: true })
+      } else {
+        this.$router.push({ name: 'contest-details', params: { contestID: item.id } })
+      }
+    },
+    getDuration (startTime, endTime) {
+      return time.duration(startTime, endTime)
+    },
+    getFormat (value) {
+      return time.utcToLocal(value, 'YYYY-M-D HH:mm')
+    },
+    remainTime (startTime) {
+      const today = new Date()
+      return time.duration(today, startTime)
+    }
+  },
   computed: {
-    rows () {
-      return this.items.length
+    ...mapState({
+      showMenu: state => state.contest.itemVisible.menu,
+      contest: state => state.contest.contest,
+      contest_table: state => [state.contest.contest],
+      now: state => state.contest.now
+    }),
+    ...mapGetters(
+      ['contestMenuDisabled', 'countdown', 'isContestAdmin',
+        'OIContestRealTimePermission', 'passwordFormVisible']
+    )
+  },
+  watch: {
+    '$route' (newVal, oldVal) {
+      if (newVal !== oldVal) {
+        this.init()
+      }
     }
   }
+
 }
 </script>
 
