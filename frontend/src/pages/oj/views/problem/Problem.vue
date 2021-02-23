ac1837af57e7d9308240eb4347ff1d6f084712a9
kangrnin <kangrnin@gmail.com>
2021-02-23 10:50:18 +0900

    Problem page Separate Sidebar in ProblemSidebar.vue

--- frontend/src/pages/oj/views/problem/Problem.vue
+++ frontend/src/pages/oj/views/problem/Problem.vue
@@ -189,97 +189,27 @@
     </b-row>
     <b-sidebar id="sidebar" no-header backdrop>
       <template #default="{ hide }">
-        <div id="sidebar-container">
-          <b-container>
-            <b-row class="sidebar-row bottom-border">
-              <b-icon icon="x" scale="2.2" @click="hide"/>
-            </b-row>
-            <b-row class="sidebar-row bottom-border" v-if="$route.name && $route.name.indexOf('contest') != -1">
-              <h2>
-                <b-icon class="sidebar-icon" icon="hash" scale="1.3" shift-v="1"/>
-                Problem List
-              </h2>
-              <ul id="problem-list">
-                <li v-for="(contestProblem, index) of contestProblems" :key="index"
-                  @click="()=>goContestProblem(contestProblem._id)">
-                  {{contestProblem.title}}
-                </li>
-              </ul>
-            </b-row>
-            <b-row class="sidebar-row">
-              <h2 v-b-modal.clarifications-modal>
-                <b-icon class="sidebar-icon" icon="question-circle" scale="1.2"/>
-                Clarification
-              </h2>
-              <h2 v-b-modal.my-submissions-modal>
-                <b-icon class="sidebar-icon" icon="person" scale="1.2"/>
-                My Submissions
-              </h2>
-              <h2 v-b-modal.all-submissions-modal>
-                <b-icon class="sidebar-icon" icon="people" scale="1.2"/>
-                All Submissions
-              </h2>
-              <h2>
-                <b-icon class="sidebar-icon" icon="bar-chart-line" scale="1.2"/>
-                Standings
-              </h2>
-            </b-row>
-          </b-container>
-        </div>
+        <ProblemSidebar :hide="hide" :contestID="contestID" :problemID="problemID"/>
       </template>
     </b-sidebar>
-
-    <div id="modal-wrapper">
-      <b-modal id="clarifications-modal" centered hide-backdrop hide-footer>
-        <template #modal-header="{ close }">
-          <div class="modal-title-close">
-            <h1>Clarifications</h1>
-            <b-icon icon="x" scale="3" shift-v="-3" @click="close()"/>
-          </div>
-        </template>
-        <div id="clarifications-table">
-          <b-table :items="clarifications"></b-table>
-        </div>
-      </b-modal>
-      <b-modal id="my-submissions-modal" centered hide-backdrop hide-footer>
-        <template #modal-header="{ close }">
-          <div class="modal-title-close">
-            <h1>My Submissions</h1>
-            <b-icon icon="x" scale="3" shift-v="-3" @click="close()"/>
-          </div>
-        </template>
-        <div id="my-submissions-table">
-          <b-table class="align-center" :items="my_submissions"></b-table>
-        </div>
-      </b-modal>
-
-      <b-modal id="all-submissions-modal" centered hide-backdrop hide-footer>
-        <template #modal-header="{ close }">
-          <div class="modal-title-close">
-            <h1>All Submissions</h1>
-            <b-icon icon="x" scale="3" shift-v="-3" @click="close()"/>
-          </div>
-        </template>
-        <div id="all-submissions-table">
-          <b-table class="align-center" :items="all_submissions"></b-table>
-        </div>
-      </b-modal>
-    </div>
   </div>
 </template>
 
 <script>
-import { mapState, mapGetters, mapActions } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
 import { types } from '../../../../store'
 import CodeMirror from '@oj/components/CodeMirror.vue'
 import storage from '@/utils/storage'
 import { FormMixin } from '@oj/components/mixins'
 import { JUDGE_STATUS, CONTEST_STATUS, buildProblemCodeKey } from '@/utils/constants'
 import api from '@oj/api'
+import ProblemSidebar from './ProblemSidebar.vue'
+
 export default {
   name: 'Problem Details',
   components: {
-    CodeMirror
+    CodeMirror,
+    ProblemSidebar
   },
   mixins: [FormMixin],
   data () {
@@ -293,10 +223,7 @@ export default {
       contestID: '',
       problemID: '',
       submitting: false,
-      code: '',
-      language: 'C++',
-      theme: 'material',
-      theme_list: ['solarized', 'monokai', 'material'],
+
       submissionId: '',
       submitted: false,
       result: {
@@ -315,10 +242,12 @@ export default {
         tags: [],
         io_mode: { io_mode: 'Standard IO' }
       },
-      // Siderbar content
-      my_submissions: [],
-      all_submissions: [],
-      clarifications: []
+
+      // CodeMirror
+      code: '',
+      language: 'C++',
+      theme: 'material',
+      theme_list: ['solarized', 'monokai', 'material']
     }
   },
   beforeRouteEnter (to, from, next) {
@@ -336,7 +265,6 @@ export default {
   mounted () {
     this.$store.commit(types.CHANGE_CONTEST_ITEM_VISIBLE, { menu: false })
     this.init()
-    this.getContestProblems()
   },
   methods: {
     ...mapActions(['changeDomTitle']),
@@ -498,17 +426,6 @@ export default {
         submitFunc(data, true)
       }
     },
-    getContestProblems () {
-      this.$store.dispatch('getContestProblems').then(res => {
-        if (this.isAuthenticated) {
-          if (this.contestRuleType === 'ACM') {
-            this.addStatusColumn(this.ACMTableColumns, res.data.data)
-          } else if (this.OIContestRealTimePermission) {
-            this.addStatusColumn(this.ACMTableColumns, res.data.data)
-          }
-        }
-      })
-    },
     goContestProblem (problemID) {
       this.$router.push({
         name: 'contest-problem-details',
@@ -520,12 +437,7 @@ export default {
     }
   },
   computed: {
-    // from ContestProblemList.vue
-    ...mapState({
-      contestProblems: state => state.contest.contestProblems
-    }),
     ...mapGetters(['isAuthenticated', 'contestRuleType', 'OIContestRealTimePermission']),
-
     ...mapGetters(['problemSubmitDisabled', 'contestRuleType', 'OIContestRealTimePermission', 'contestStatus']),
     contest () {
       return this.$store.state.contest.contest
@@ -567,6 +479,14 @@ export default {
 </script>
 
 <style lang="less" scoped>
+@font-face {
+  font-family: 'Manrope';
+  src: url("../../../../fonts/Manrope-Bold.ttf");
+}
+* {
+  font-family: 'Manrope', sans-serif;
+}
+
 #container {
   display: flex;
   flex-flow: column;
@@ -712,113 +632,6 @@ export default {
 
         .io-content-cell {
           padding: 10px 15px;
-        }
-      }
-    }
-  }
-}
-
-#sidebar-container {
-  height: 100%;
-  background: #24272D;
-  color: white;
-
-  .sidebar-row {
-    padding: 15px 15px;
-    padding-left: 25px;
-
-    .sidebar-icon {
-      margin-right: 7px;
-    }
-
-    h2 {
-      width: 100%;
-      margin-top: 10px;
-      margin-bottom: 20px;
-      font-size: 18px;
-    }
-
-    #problem-list {
-      margin-left: 30px;
-
-      li {
-        list-style-type: none;
-        margin-top: 10px;
-        margin-bottom: 20px;
-        font-size: 18px;
-      }
-    }
-  }
-
-  .bottom-border {
-    border-bottom: 1px solid #3B4F56;
-  }
-}
-
-/deep/ .modal {
-  .modal-dialog {
-    min-width: 1200px;
-
-    .modal-content {
-      border-radius: 10px;
-      background: #24272D;
-      color: white;
-
-      .modal-header {
-        border-bottom: none;
-
-        .modal-title-close {
-          width: 100%;
-          display: flex;
-          flex-direction: row;
-          justify-content: space-between;
-
-          h1 {
-            display: inline-block;
-            margin-top: 10px;
-            margin-left: 10px;
-            font-size: 35px;
-          }
-        }
-      }
-
-      .modal-body {
-        padding: 0;
-
-        #clarifications-table {
-          table {
-            color: white;
-
-            th {
-              min-width: 230px;
-              padding: 15px 25px;
-              border: none;
-            }
-
-            td {
-              min-width: 230px;
-              padding: 15px 25px;
-              border-top: 1px solid #3B4F56;
-            }
-          }
-        }
-
-        #my-submissions-table, #all-submissions-table {
-          table {
-            color: white;
-
-            th {
-              min-width: 100px;
-              padding: 15px 25px;
-              border: none;
-            }
-
-            td {
-              min-width: 100px;
-              padding: 15px 25px;
-              border-top: 1px solid #3B4F56;
-            }
-          }
         }
       }
     }
---
+++ frontend/src/pages/oj/views/problem/ProblemSidebar.vue
@@ -0,0 +1,230 @@
+<template>
+  <div id="sidebar-container">
+    <b-container>
+      <b-row class="sidebar-row bottom-border">
+        <b-icon icon="x" scale="2.2" @click="hide"/>
+      </b-row>
+      <b-row class="sidebar-row bottom-border" v-if="$route.name && $route.name.indexOf('contest') != -1">
+        <h2>
+          <b-icon class="sidebar-icon" icon="hash" scale="1.3" shift-v="1"/>
+          Problem List
+        </h2>
+        <ul id="problem-list">
+          <li v-for="(contestProblem, index) of contestProblems" :key="index"
+            @click="()=>goContestProblem(contestProblem._id)">
+            {{contestProblem.title}}
+          </li>
+        </ul>
+      </b-row>
+      <b-row class="sidebar-row">
+        <h2 v-b-modal.clarifications-modal>
+          <b-icon class="sidebar-icon" icon="question-circle" scale="1.2"/>
+          Clarification
+        </h2>
+        <h2 v-b-modal.my-submissions-modal>
+          <b-icon class="sidebar-icon" icon="person" scale="1.2"/>
+          My Submissions
+        </h2>
+        <h2 v-b-modal.all-submissions-modal>
+          <b-icon class="sidebar-icon" icon="people" scale="1.2"/>
+          All Submissions
+        </h2>
+        <h2>
+          <b-icon class="sidebar-icon" icon="bar-chart-line" scale="1.2"/>
+          Standings
+        </h2>
+      </b-row>
+    </b-container>
+
+    <div id="modal-wrapper">
+      <b-modal id="clarifications-modal" centered hide-backdrop hide-footer>
+        <template #modal-header="{ close }">
+          <div class="modal-title-close">
+            <h1>Clarifications</h1>
+            <b-icon icon="x" scale="3" shift-v="-3" @click="close()"/>
+          </div>
+        </template>
+        <div id="clarifications-table">
+          <b-table :items="clarifications"></b-table>
+        </div>
+      </b-modal>
+      <b-modal id="my-submissions-modal" centered hide-backdrop hide-footer>
+        <template #modal-header="{ close }">
+          <div class="modal-title-close">
+            <h1>My Submissions</h1>
+            <b-icon icon="x" scale="3" shift-v="-3" @click="close()"/>
+          </div>
+        </template>
+        <div id="my-submissions-table">
+          <b-table class="align-center" :items="my_submissions"></b-table>
+        </div>
+      </b-modal>
+
+      <b-modal id="all-submissions-modal" centered hide-backdrop hide-footer>
+        <template #modal-header="{ close }">
+          <div class="modal-title-close">
+            <h1>All Submissions</h1>
+            <b-icon icon="x" scale="3" shift-v="-3" @click="close()"/>
+          </div>
+        </template>
+        <div id="all-submissions-table">
+          <b-table class="align-center" :items="all_submissions"></b-table>
+        </div>
+      </b-modal>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'ProblemSidebar',
+  components: {
+
+  },
+  props: {
+    hide: Function,
+    contestID: String,
+    problemID: String
+  },
+  data () {
+    return {
+      my_submissions: [],
+      all_submissions: [],
+      clarifications: []
+    }
+  },
+  mounted () {
+    this.getContestProblems()
+  },
+  methods: {
+    getContestProblems () {
+      this.$store.dispatch('getContestProblems').then(res => {
+        if (this.isAuthenticated) {
+          if (this.contestRuleType === 'ACM') {
+            this.addStatusColumn(this.ACMTableColumns, res.data.data)
+          } else if (this.OIContestRealTimePermission) {
+            this.addStatusColumn(this.ACMTableColumns, res.data.data)
+          }
+        }
+      })
+    }
+  },
+  computed: {
+    ...mapState({
+      contestProblems: state => state.contest.contestProblems
+    })
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+#sidebar-container {
+  height: 100%;
+  background: #24272D;
+  color: white;
+
+  .sidebar-row {
+    padding: 15px 15px;
+    padding-left: 25px;
+
+    .sidebar-icon {
+      margin-right: 7px;
+    }
+
+    h2 {
+      width: 100%;
+      margin-top: 10px;
+      margin-bottom: 20px;
+      font-size: 18px;
+    }
+
+    #problem-list {
+      margin-left: 30px;
+
+      li {
+        list-style-type: none;
+        margin-top: 10px;
+        margin-bottom: 20px;
+        font-size: 18px;
+      }
+    }
+  }
+
+  .bottom-border {
+    border-bottom: 1px solid #3B4F56;
+  }
+}
+
+/deep/ .modal {
+  .modal-dialog {
+    min-width: 1200px;
+
+    .modal-content {
+      border-radius: 10px;
+      background: #24272D;
+      color: white;
+
+      .modal-header {
+        border-bottom: none;
+
+        .modal-title-close {
+          width: 100%;
+          display: flex;
+          flex-direction: row;
+          justify-content: space-between;
+
+          h1 {
+            display: inline-block;
+            margin-top: 10px;
+            margin-left: 10px;
+            font-size: 35px;
+          }
+        }
+      }
+
+      .modal-body {
+        padding: 0;
+
+        #clarifications-table {
+          table {
+            color: white;
+
+            th {
+              min-width: 230px;
+              padding: 15px 25px;
+              border: none;
+            }
+
+            td {
+              min-width: 230px;
+              padding: 15px 25px;
+              border-top: 1px solid #3B4F56;
+            }
+          }
+        }
+
+        #my-submissions-table, #all-submissions-table {
+          table {
+            color: white;
+
+            th {
+              min-width: 100px;
+              padding: 15px 25px;
+              border: none;
+            }
+
+            td {
+              min-width: 100px;
+              padding: 15px 25px;
+              border-top: 1px solid #3B4F56;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</style>
