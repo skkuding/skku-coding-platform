a0dc64b2c1539ca9766843fd8faa0fe1ceb8a8c2
kangrnin <kangrnin@gmail.com>
2021-03-03 12:19:58 +0900

    Home panel title text overflow ellipsis
    - 추가로 패널 테이블 col width 조정

--- frontend/src/pages/oj/views/general/Home.vue
+++ frontend/src/pages/oj/views/general/Home.vue
@@ -10,11 +10,16 @@
           </div>
           <b-icon icon="list" shift-v="-4" @click="goAnnouncement()"/>
         </div>
-        <b-table borderless
+        <b-table borderless fixed
           :items="announcements"
           :fields="announcement_fields"
           @row-clicked="goAnnouncement"
         >
+          <template #table-colgroup="">
+            <col style="width: 40px">
+            <col style="width: 100%">
+            <col style="width: 125px">
+          </template>
           <template #cell(icon)>
             <b-icon icon="chevron-right"/>
           </template>
@@ -31,11 +36,16 @@
           </div>
           <b-icon icon="list" shift-v="-4" @click="goContestList()"/>
         </div>
-        <b-table borderless
+        <b-table borderless fixed
           :items="contests"
           :fields="contest_fields"
           @row-clicked="goContest"
         >
+          <template #table-colgroup>
+            <col style="width: 40px">
+            <col style="width: 100%">
+            <col style="width: 125px">
+          </template>
           <template #cell(icon)="data">
             <b-icon :icon="data.item.status === '0' ? 'three-dots' : 'calendar2'"/>
           </template>
@@ -69,31 +79,41 @@ export default {
       announcement_fields: [
         {
           label: 'icon',
-          key: 'icon'
+          key: 'icon',
+          tdClass: 'icon-field',
+          thClass: 'icon-field'
         },
         {
           label: 'title',
-          key: 'title'
+          key: 'title',
+          tdClass: 'title-field',
+          thClass: 'title-field'
         },
         {
           label: 'create_time',
           key: 'create_time',
-          tdClass: 'date-field'
+          tdClass: 'date-field',
+          thClass: 'date-field'
         }
       ],
       contest_fields: [
         {
           label: 'icon',
-          key: 'icon'
+          key: 'icon',
+          tdClass: 'icon-field',
+          thClass: 'icon-field'
         },
         {
           label: 'title',
-          key: 'title'
+          key: 'title',
+          tdClass: 'title-field',
+          thClass: 'title-field'
         },
         {
           label: 'start_time',
           key: 'start_time',
-          tdClass: 'date-field'
+          tdClass: 'date-field',
+          thClass: 'date-field'
         }
       ]
     }
@@ -209,24 +229,32 @@ export default {
   }
 }
 
-/deep/ .table tr {
-  outline: none;
-  font-size: 16px;
+/deep/ .table {
+  tr {
+    outline: none;
+    font-size: 16px;
+  }
 
-  th {
+  thead {
     display: none;
   }
 
-  td {
-    max-width: 60%;
+  & .icon-field {
+    // width: 40px;
+  }
+
+  & .title-field {
+    width: 100%;
+    display: inline-block;
     text-overflow: ellipsis;
     overflow: hidden;
     white-space: nowrap;
   }
-}
 
-/deep/ .table .date-field {
-  text-align:right;
+  & .date-field {
+    // width: 125px;
+    text-align: right;
+  }
 }
 
 @media screen and (max-width: 1016px) {
