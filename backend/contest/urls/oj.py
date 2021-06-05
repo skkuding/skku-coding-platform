77507b64b11e6dbda85299c95c0dee2abcf9be38
Jaemin Choi <1dotolee@gmail.com>
2021-06-05 16:45:02 +0900

    Use `path()` for url patterns
    `url(r"")`은 구버전의 Django 방식으로, `path()`로 대체.
    추후 regex 필요 시 `re_path()` 사용.

--- backend/contest/urls/oj.py
+++ backend/contest/urls/oj.py
@@ -1,13 +1,13 @@
-from django.conf.urls import url
+from django.urls import path
 
 from ..views.oj import ContestAnnouncementListAPI
 from ..views.oj import ContestPasswordVerifyAPI, ContestAccessAPI
 from ..views.oj import ContestListAPI, ContestAPI
 
 urlpatterns = [
-    url(r"^contests/?$", ContestListAPI.as_view(), name="contest_list_api"),
-    url(r"^contest/?$", ContestAPI.as_view(), name="contest_api"),
-    url(r"^contest/password/?$", ContestPasswordVerifyAPI.as_view(), name="contest_password_api"),
-    url(r"^contest/announcement/?$", ContestAnnouncementListAPI.as_view(), name="contest_announcement_api"),
-    url(r"^contest/access/?$", ContestAccessAPI.as_view(), name="contest_access_api"),
+    path("contests/", ContestListAPI.as_view(), name="contest_list_api"),
+    path("contest/", ContestAPI.as_view(), name="contest_api"),
+    path("contest/password/", ContestPasswordVerifyAPI.as_view(), name="contest_password_api"),
+    path("contest/announcement/", ContestAnnouncementListAPI.as_view(), name="contest_announcement_api"),
+    path("contest/access/", ContestAccessAPI.as_view(), name="contest_access_api"),
 ]
--- backend/problem/urls/oj.py
+++ backend/problem/urls/oj.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls import path
 
 from ..views.oj import ProblemTagAPI, ProblemAPI, ContestProblemAPI
 
 urlpatterns = [
-    url(r"^problem/tags/?$", ProblemTagAPI.as_view(), name="problem_tag_list_api"),
-    url(r"^problem/?$", ProblemAPI.as_view(), name="problem_api"),
-    url(r"^contest/problem/?$", ContestProblemAPI.as_view(), name="contest_problem_api"),
+    path("problem/tags/", ProblemTagAPI.as_view(), name="problem_tag_list_api"),
+    path("problem/", ProblemAPI.as_view(), name="problem_api"),
+    path("contest/problem/", ContestProblemAPI.as_view(), name="contest_problem_api"),
 ]
