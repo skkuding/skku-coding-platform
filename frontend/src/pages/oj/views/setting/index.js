29284bf3c448cf866f327cc8177a9c886d9daeeb
HAKSAN <cranemont@gmail.com>
2021-06-16 17:27:29 +0900

    Move ProfileSetting.vue to oj/views/user
    - Remove oj/views/settings/*
    - Fix import path of ProfileSetting.vue from Header.vue and Problem.vue
    - Fix ProfileSetting access path from routes.js

--- frontend/src/pages/oj/views/setting/index.js
+++
@@ -1,4 +0,0 @@
-const Settings = () => import(/* webpackChunkName: "setting" */ './Settings.vue')
-const ProfileSetting = () => import(/* webpackChunkName: "setting" */ './children/ProfileSetting.vue')
-
-export { Settings, ProfileSetting }
