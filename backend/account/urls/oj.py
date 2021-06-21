ae9cdf1fca386601043a12081d104c1218132b4d
joft-ware <zxc91911003@gmail.com>
2021-06-21 13:36:32 +0900

    deleted SessionManagementAPI again

--- backend/account/urls/oj.py
+++ backend/account/urls/oj.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from ..views.oj import (ApplyResetPasswordAPI, ResetPasswordAPI,
                         UserChangePasswordAPI, UserRegisterAPI, EmailAuthAPI, UserChangeEmailAPI,
                         UserLoginAPI, UserLogoutAPI, UsernameOrEmailCheck,
-                        AvatarUploadAPI, UserProfileAPI, UserRankAPI, SessionManagementAPI, OpenAPIAppkeyAPI, SSOAPI, UserSettingAPI)
+                        AvatarUploadAPI, UserProfileAPI, UserRankAPI, OpenAPIAppkeyAPI, SSOAPI, UserSettingAPI)
 
 from utils.captcha.views import CaptchaAPIView
 
@@ -22,7 +22,6 @@ urlpatterns = [
     url(r"^user/?$", UserSettingAPI.as_view(), name="user_setting_api"),
     url(r"^upload_avatar/?$", AvatarUploadAPI.as_view(), name="avatar_upload_api"),
     url(r"^user_rank/?$", UserRankAPI.as_view(), name="user_rank_api"),
-    url(r"^sessions/?$", SessionManagementAPI.as_view(), name="session_management_api"),
     url(r"^open_api_appkey/?$", OpenAPIAppkeyAPI.as_view(), name="open_api_appkey_api"),
     url(r"^sso?$", SSOAPI.as_view(), name="sso_api")
 ]
