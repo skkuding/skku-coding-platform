f833173230a58f77725b31dfef87e5b30f9c5fb9
DailyLinux <baksa1849@gmail.com>
2021-06-26 00:28:29 +0900

    Added menu bar to Tiptap editor.

--- frontend/src/pages/admin/components/Tiptap.vue
+++ frontend/src/pages/admin/components/Tiptap.vue
@@ -1,73 +1,61 @@
 <template>
   <div>
     <div v-if="editor">
-      <button type="button" @click="editor.chain().focus().toggleBold().run()" :class="{ 'is-active': editor.isActive('bold') }">
-        bold
-      </button>
-      <button type="button" @click="editor.chain().focus().toggleItalic().run()" :class="{ 'is-active': editor.isActive('italic') }">
-        italic
-      </button>
-      <button type="button" @click="editor.chain().focus().toggleStrike().run()" :class="{ 'is-active': editor.isActive('strike') }">
-        strike
-      </button>
-      <button type="button" @click="editor.toggleCode()" :class="{ 'is-active': editor.isActive('code') }">
-        code
-      </button>
-      <button type="button" @click="editor.chain().focus().unsetAllMarks().run()">
-        clear marks
-      </button>
-      <button type="button" @click="editor.chain().focus().clearNodes().run()">
-        clear nodes
-      </button>
-      <button type="button" @click="editor.chain().focus().setParagraph().run()" :class="{ 'is-active': editor.isActive('paragraph') }">
-        paragraph
-      </button>
-      <button type="button" @click="editor.chain().focus().toggleHeading({ level: 1 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }">
-        h1
-      </button>
-      <button type="button" @click="editor.chain().focus().toggleHeading({ level: 2 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }">
-        h2
-      </button>
-      <button type="button" @click="editor.chain().focus().toggleHeading({ level: 3 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 3 }) }">
-        h3
-      </button>
-      <button type="button" @click="editor.chain().focus().toggleHeading({ level: 4 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 4 }) }">
-        h4
-      </button>
-      <button type="button" @click="editor.chain().focus().toggleHeading({ level: 5 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 5 }) }">
-        h5
-      </button>
-      <button type="button" @click="editor.chain().focus().toggleHeading({ level: 6 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 6 }) }">
-        h6
-      </button>
-      <button type="button" @click="editor.chain().focus().toggleBulletList().run()" :class="{ 'is-active': editor.isActive('bulletList') }">
-        bullet list
-      </button>
-      <button type="button" @click="editor.chain().focus().toggleOrderedList().run()" :class="{ 'is-active': editor.isActive('orderedList') }">
-        ordered list
-      </button>
-      <button type="button" @click="editor.chain().focus().toggleCodeBlock().run()" :class="{ 'is-active': editor.isActive('codeBlock') }">
-        code block
-      </button>
-      <button type="button" @click="editor.chain().focus().toggleBlockquote().run()" :class="{ 'is-active': editor.isActive('blockquote') }">
-        blockquote
-      </button>
-      <button type="button" @click="editor.chain().focus().setHorizontalRule().run()">
-        horizontal rule
-      </button>
-      <button type="button" @click="editor.chain().focus().setHardBreak().run()">
-        hard break
-      </button>
-      <button type="button" @click="editor.chain().focus().undo().run()">
-        undo
-      </button>
-      <button type="button" @click="editor.chain().focus().redo().run()">
-        redo
-      </button>
+      <b-button-toolbar>
+        <b-button-group>
+          <b-button type="button" variant="outline-dark" @click="editor.chain().focus().toggleBold().run()" :class="{ 'is-active': editor.isActive('bold') }">
+            <b-icon icon="type-bold" aria-hidden="true"></b-icon>
+          </b-button>
+          <b-button type="button" variant="outline-dark" @click="editor.chain().focus().toggleItalic().run()" :class="{ 'is-active': editor.isActive('italic') }">
+            <b-icon icon="type-italic" aria-hidden="true"></b-icon>
+          </b-button>
+          <b-button type="button" variant="outline-dark" @click="editor.chain().focus().toggleStrike().run()" :class="{ 'is-active': editor.isActive('strike') }">
+            <b-icon icon="type-strikethrough" aria-hidden="true"></b-icon>
+          </b-button>
+          <b-button type="button" variant="outline-dark" @click="editor.chain().focus().toggleCode().run()" :class="{ 'is-active': editor.isActive('code') }">
+            <b-icon icon="code" aria-hidden="true"></b-icon>
+          </b-button>
+        </b-button-group>
+        <b-button-group >
+          <b-button type="button" variant="outline-dark" @click="editor.chain().focus().setParagraph().run()" :class="{ 'is-active': editor.isActive('paragraph') }">
+            <b-icon icon="paragraph" aria-hidden="true"></b-icon>
+          </b-button>
+          <b-button type="button" variant="outline-dark" @click="editor.chain().focus().toggleHeading({ level: 1 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }">
+            <b-icon icon="type-h1" aria-hidden="true"></b-icon>
+          </b-button>
+          <b-button type="button" variant="outline-dark" @click="editor.chain().focus().toggleHeading({ level: 2 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }">
+            <b-icon icon="type-h2" aria-hidden="true"></b-icon>
+          </b-button>
+          <b-button type="button" variant="outline-dark" @click="editor.chain().focus().toggleHeading({ level: 3 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 3 }) }">
+            <b-icon icon="type-h3" aria-hidden="true"></b-icon>
+          </b-button>
+          <b-button type="button" variant="outline-dark" @click="editor.chain().focus().toggleBulletList().run()" :class="{ 'is-active': editor.isActive('bulletList') }">
+            <b-icon icon="list-ul" aria-hidden="true"></b-icon>
+          </b-button>
+          <b-button type="button" variant="outline-dark" @click="editor.chain().focus().toggleOrderedList().run()" :class="{ 'is-active': editor.isActive('orderedList') }">
+            <b-icon icon="list-ol" aria-hidden="true"></b-icon>
+        </b-button>
+        </b-button-group>
+        <b-button-group >
+          <b-button type="button" variant="outline-dark" @click="editor.chain().focus().toggleCodeBlock().run()" :class="{ 'is-active': editor.isActive('codeBlock') }">
+            <b-icon icon="code-square" aria-hidden="true"></b-icon>
+          </b-button>
+          <b-button type="button" variant="outline-dark" @click="editor.chain().focus().toggleBlockquote().run()" :class="{ 'is-active': editor.isActive('blockquote') }">
+            <b-icon icon="blockquote-left" aria-hidden="true"></b-icon>
+          </b-button>
+          <b-button type="button" variant="outline-dark" @click="editor.chain().focus().setHorizontalRule().run()">
+            <b-icon icon="distribute-horizontal" aria-hidden="true"></b-icon>
+          </b-button>
+          <b-button type="button" variant="outline-dark" @click="editor.chain().focus().undo().run()">
+            <b-icon icon="arrow-counterclockwise" aria-hidden="true"></b-icon>
+          </b-button>
+          <b-button type="button" variant="outline-dark" @click="editor.chain().focus().redo().run()">
+            <b-icon icon="arrow-clockwise" aria-hidden="true"></b-icon>
+          </b-button>
+        </b-button-group>
+      </b-button-toolbar>
     </div>
-    <div :class="el-input">
-      <editor-content :editor="editor" :class="el-input"/>
-    </div>
+    <editor-content :editor="editor" />
   </div>
 </template>
 
@@ -92,16 +80,10 @@ export default {
   },
   watch: {
     value (value) {
-      // HTML
       const isSame = this.editor.getHTML() === value
-
-      // JSON
-      // const isSame = this.editor.getJSON().toString() === value.toString()
-
       if (isSame) {
         return
       }
-
       this.editor.commands.setContent(this.value, false)
     }
   },
@@ -112,11 +94,7 @@ export default {
       ],
       content: this.value,
       onUpdate: () => {
-        // HTML
         this.$emit('input', this.editor.getHTML())
-
-        // JSON
-        // this.$emit('input', this.editor.getJSON())
       }
     })
   },
@@ -126,64 +104,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss">
-/* Basic editor styles */
-.ProseMirror {
-  padding: 1rem;
-
-  > * + * {
-    margin-top: 0.75em;
-  }
-
-  ul,
-  ol {
-    padding: 0 1rem;
-  }
-
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6 {
-    line-height: 1.1;
-  }
-
-  code {
-    background-color: rgba(#616161, 0.1);
-    color: #616161;
-  }
-
-  pre {
-    background: #0D0D0D;
-    color: #FFF;
-    font-family: 'JetBrainsMono', monospace;
-    padding: 0.75rem 1rem;
-    border-radius: 0.5rem;
-
-    code {
-      color: inherit;
-      padding: 0;
-      background: none;
-      font-size: 0.8rem;
-    }
-  }
-
-  img {
-    max-width: 100%;
-    height: auto;
-  }
-
-  blockquote {
-    padding-left: 1rem;
-    border-left: 2px solid rgba(#0D0D0D, 0.1);
-  }
-
-  hr {
-    border: none;
-    border-top: 2px solid rgba(#0D0D0D, 0.1);
-    margin: 2rem 0;
-  }
-}
-</style>
