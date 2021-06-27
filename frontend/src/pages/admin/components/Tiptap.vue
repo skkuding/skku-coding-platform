5f99ebf2d0b4f64a91199984dc89237e67ce9801
Jimin Ha <jimin9038@g.skku.edu>
2021-06-28 03:56:00 +0900

    Tiptap add Link, Insert Image/File + ... feature
    - Add 'Unset Marks', 'Hard Break' feature
    - Add 'Link', 'Unset Marks' on Bubble Menu
    
    - Add 'Link', 'Insert Image', 'Insert File'
    - Add Modal for 'Insert Image' and 'Insert File'
    - Add Dependencies for 'Link', 'Insert Image'
    
    - Add Divider

--- frontend/src/pages/admin/components/Tiptap.vue
+++ frontend/src/pages/admin/components/Tiptap.vue
@@ -4,82 +4,144 @@
       <b-button-toolbar class="mb-1">
         <b-button-group class="mr-2">
           <b-button size="sm" title="Bold(Ctrl+B/Cmd+B)" type="button" variant="outline-dark" @click="editor.chain().focus().toggleBold().run()" class="EditorButton" :class="{ 'is-active': editor.isActive('bold') }">
-            <b-icon icon="type-bold" aria-hidden="true"></b-icon>
+            <b-icon icon="type-bold"></b-icon>
           </b-button>
           <b-button size="sm" title="Italic(Ctrl+I/Cmd+i)" type="button" variant="outline-dark" @click="editor.chain().focus().toggleItalic().run()" class="EditorButton" :class="{ 'is-active': editor.isActive('italic') }">
-            <b-icon icon="type-italic" aria-hidden="true"></b-icon>
+            <b-icon icon="type-italic"></b-icon>
           </b-button>
-          <b-button size="sm" title="Strike(Ctrl+B/Cmd+B)" type="button" variant="outline-dark" @click="editor.chain().focus().toggleStrike().run()" class="EditorButton" :class="{ 'is-active': editor.isActive('strike') }">
-            <b-icon icon="type-strikethrough" aria-hidden="true"></b-icon>
+          <b-button size="sm" title="Strike" type="button" variant="outline-dark" @click="editor.chain().focus().toggleStrike().run()" class="EditorButton" :class="{ 'is-active': editor.isActive('strike') }">
+            <b-icon icon="type-strikethrough"></b-icon>
           </b-button>
           <b-button size="sm" title="Code" type="button" variant="outline-dark" @click="editor.chain().focus().toggleCode().run()" class="EditorButton" :class="{ 'is-active': editor.isActive('code') }">
-            <b-icon icon="code" aria-hidden="true"></b-icon>
+            <b-icon icon="code"></b-icon>
+          </b-button>
+          <b-button size="sm" title="Unset Marks" type="button" variant="outline-dark" @click="editor.chain().focus().unsetAllMarks().run()">
+            <b-icon icon="x-square"></b-icon>
           </b-button>
         </b-button-group>
         <div class="divider"></div>
         <b-button-group class="mx-2">
           <b-button size="sm" title="Heading 3" type="button" variant="outline-dark" @click="editor.chain().focus().toggleHeading({ level: 3 }).run()" class="EditorButton" :class="{ 'is-active': editor.isActive('heading', { level: 3 }) }">
-            <b-icon icon="type-h1" aria-hidden="true"></b-icon>
+            <b-icon icon="type-h1"></b-icon>
           </b-button>
           <b-button size="sm" title="Heading 4" type="button" variant="outline-dark" @click="editor.chain().focus().toggleHeading({ level: 4 }).run()" class="EditorButton" :class="{ 'is-active': editor.isActive('heading', { level: 4 }) }">
-            <b-icon icon="type-h2" aria-hidden="true"></b-icon>
+            <b-icon icon="type-h2"></b-icon>
           </b-button>
           <b-button size="sm" title="Paragraph" type="button" variant="outline-dark" @click="editor.chain().focus().setParagraph().run()" class="EditorButton" :class="{ 'is-active': editor.isActive('paragraph') }">
-            <b-icon icon="paragraph" aria-hidden="true"></b-icon>
+            <b-icon icon="paragraph"></b-icon>
           </b-button>
           <b-button size="sm" title="Bullet List" type="button" variant="outline-dark" @click="editor.chain().focus().toggleBulletList().run()" class="EditorButton" :class="{ 'is-active': editor.isActive('bulletList') }">
-            <b-icon icon="list-ul" aria-hidden="true"></b-icon>
+            <b-icon icon="list-ul"></b-icon>
           </b-button>
           <b-button size="sm" title="Ordered List" type="button" variant="outline-dark" @click="editor.chain().focus().toggleOrderedList().run()" class="EditorButton" :class="{ 'is-active': editor.isActive('orderedList') }">
-            <b-icon icon="list-ol" aria-hidden="true"></b-icon>
+            <b-icon icon="list-ol"></b-icon>
           </b-button>
           <b-button size="sm" name="Code Block" type="button" variant="outline-dark" @click="editor.chain().focus().toggleCodeBlock().run()" class="EditorButton" :class="{ 'is-active': editor.isActive('codeBlock') }">
-            <b-icon icon="code-square" aria-hidden="true"></b-icon>
+            <b-icon icon="code-square"></b-icon>
           </b-button>
         </b-button-group>
         <div class="divider"></div>
         <b-button-group class="mx-2">
+          <b-button size="sm" title="Hard Break(Ctrl+Enter/Cmd+Enter)" type="button" variant="outline-dark" @click="editor.chain().focus().setHardBreak().run()">
+            <b-icon icon="arrow-return-left"></b-icon>
+          </b-button>
           <b-button size="sm" title="Block Quote" type="button" variant="outline-dark" @click="editor.chain().focus().toggleBlockquote().run()" class="EditorButton" :class="{ 'is-active': editor.isActive('blockquote') }">
-            <b-icon icon="blockquote-left" aria-hidden="true"></b-icon>
+            <b-icon icon="blockquote-left"></b-icon>
           </b-button>
           <b-button size="sm" title="Horizontal Rule" type="button" variant="outline-dark" @click="editor.chain().focus().setHorizontalRule().run()">
-            <b-icon icon="distribute-vertical" aria-hidden="true"></b-icon>
+            <b-icon icon="distribute-vertical"></b-icon>
           </b-button>
         </b-button-group>
         <div class="divider"></div>
         <b-button-group class="mx-2">
           <b-button size="sm" title="Undo" type="button" variant="outline-dark" @click="editor.chain().focus().undo().run()">
-            <b-icon icon="arrow-counterclockwise" aria-hidden="true"></b-icon>
+            <b-icon icon="arrow-counterclockwise"></b-icon>
           </b-button>
           <b-button size="sm" title="Redo" type="button" variant="outline-dark" @click="editor.chain().focus().redo().run()">
-            <b-icon icon="arrow-clockwise" aria-hidden="true"></b-icon>
+            <b-icon icon="arrow-clockwise"></b-icon>
+          </b-button>
+        </b-button-group>
+        <div class="divider"></div>
+        <b-button-group class="mx-2">
+          <b-button size="sm" title="Link" type="button" variant="outline-dark" :class="{ 'is-disabled': shouldDisableButton(editor.isActive('link')), 'is-active': editor.isActive('link') }"
+                     @click.prevent="editor.isActive('link') ? editor.chain().focus().unsetLink() : addLinkDialog()">
+            <b-icon icon="link"></b-icon>
+          </b-button>
+          <b-button size="sm" title="Upload Image" type="button" variant="outline-dark" @click='openImageModal'>
+            <b-icon icon="card-image"></b-icon>
+          </b-button>
+          <b-button size="sm" title="Upload File" type="button" variant="outline-dark" @click='openFileModal'>
+            <b-icon icon="file-earmark-arrow-up"></b-icon>
           </b-button>
         </b-button-group>
       </b-button-toolbar>
     </div>
-    <div>
     <bubble-menu :editor="editor" v-if="editor">
       <b-button size="sm" title="Bold(Ctrl+B/Cmd+B)" type="button" variant="outline-dark" @click="editor.chain().focus().toggleBold().run()" class="EditorButton" :class="{ 'is-active': editor.isActive('bold') }">
-        <b-icon icon="type-bold" aria-hidden="true"></b-icon>
+        <b-icon icon="type-bold"></b-icon>
       </b-button>
       <b-button size="sm" title="Italic(Ctrl+I/Cmd+i)" type="button" variant="outline-dark" @click="editor.chain().focus().toggleItalic().run()" class="EditorButton" :class="{ 'is-active': editor.isActive('italic') }">
-        <b-icon icon="type-italic" aria-hidden="true"></b-icon>
+        <b-icon icon="type-italic"></b-icon>
       </b-button>
       <b-button size="sm" title="Strike(Ctrl+B/Cmd+B)" type="button" variant="outline-dark" @click="editor.chain().focus().toggleStrike().run()" class="EditorButton" :class="{ 'is-active': editor.isActive('strike') }">
-        <b-icon icon="type-strikethrough" aria-hidden="true"></b-icon>
+        <b-icon icon="type-strikethrough"></b-icon>
       </b-button>
       <b-button size="sm" title="Code" type="button" variant="outline-dark" @click="editor.chain().focus().toggleCode().run()" class="EditorButton" :class="{ 'is-active': editor.isActive('code') }">
-        <b-icon icon="code" aria-hidden="true"></b-icon>
+        <b-icon icon="code"></b-icon>
+      </b-button>
+      <b-button size="sm" title="Link" type="button" variant="outline-dark" :class="{ 'is-disabled': shouldDisableButton(editor.isActive('link')), 'is-active': editor.isActive('link') }"
+                  @click.prevent="editor.isActive('link') ? editor.chain().focus().unsetLink() : addLinkDialog()">
+        <b-icon icon="link"></b-icon>
+      </b-button>
+      <b-button size="sm" title="Unset Marks" type="button" variant="outline-dark" @click="editor.chain().focus().unsetAllMarks().run()" class="EditorButton">
+        <b-icon icon="x-square"></b-icon>
       </b-button>
     </bubble-menu>
     <editor-content :editor="editor" class="border" />
+    <b-modal id="image-modal" v-model="imageModal" max-width="540" title="Insert Image" hide-footer>
+      <b-card class="pa-1">
+        <b-tabs v-model="imageTab">
+          <b-tab href="#Insert-With-URL" title="Insert With URL" active>
+            <b-card-text class="mt-3 mb-3">
+              <b-form-textarea v-model="fileURL" single-line label="Paste link here">
+              </b-form-textarea>
+            </b-card-text>
+            <b-btn dark @click="insertImage(); $bvModal.hide('image-modal')" :class="{ 'is-disabled': fileURL === null }">
+              Insert image
+            </b-btn>
+          </b-tab>
+          <b-tab href="#Upload-From-Local" title="Upload From Local">
+            <b-card-text class="mt-3 mb-3">
+              <b-form-file v-model="selectedFile" accept="image/*" drop-placeholder="Drop a Image here" placeholder="Select a Image or Drop a file here" @input="uploadImage" >
+              </b-form-file>
+            </b-card-text>
+            <b-btn dark @click="insertImage(); $bvModal.hide('image-modal')" :class="{ 'is-disabled': selectedFile === null }">
+              Insert image
+            </b-btn>
+          </b-tab>
+        </b-tabs>
+      </b-card>
+    </b-modal>
+    <b-modal id="file-modal" v-model="fileModal" max-width="540" title="Insert File" hide-footer>
+      <b-card class="pa-1" >
+        <b-card-text>
+          <b-form-file v-model="selectedFile" accept="*" drop-placeholder="Drop a file here" placeholder="Select a file or Drop a file here" @input="uploadFile">
+          </b-form-file>
+        </b-card-text>
+        <b-btn type="submit" dark @click="insertFile(); $bvModal.hide('file-modal')" :class="{ 'is-disabled': selectedFile === null }" >
+          Insert File
+        </b-btn>
+      </b-card>
+    </b-modal>
   </div>
 </template>
 
 <script>
 import { Editor, EditorContent, BubbleMenu } from '@tiptap/vue-2'
 import StarterKit from '@tiptap/starter-kit'
-
+import Image from '@tiptap/extension-image'
+import Link from '@tiptap/extension-link'
+import axios from 'axios'
 export default {
   components: {
     EditorContent,
@@ -93,7 +155,13 @@ export default {
   },
   data () {
     return {
-      editor: null
+      editor: null,
+      imageModal: false,
+      imageTab: null,
+      fileModal: false,
+      fileURL: null,
+      fileLink: null,
+      selectedFile: null
     }
   },
   watch: {
@@ -108,7 +176,9 @@ export default {
   mounted () {
     this.editor = new Editor({
       extensions: [
-        StarterKit
+        StarterKit,
+        Image,
+        Link
       ],
       content: this.value,
       onUpdate: () => {
@@ -116,9 +186,77 @@ export default {
       }
     })
   },
-
   beforeDestroy () {
     this.editor.destroy()
+  },
+  methods: {
+    openImageModal: function () {
+      this.imageModal = true
+    },
+    openFileModal: function () {
+      this.fileModal = true
+    },
+    shouldDisableButton: function (isActive) {
+      return !isActive & window.getSelection().isCollapsed
+    },
+    addLinkDialog: async function () {
+      if (window.getSelection().isCollapsed) {
+        return
+      }
+      const res = await window.prompt('Add link', 'https://')
+      if (res && res !== 'https://' && res !== 'http://') {
+        this.editor.chain().focus().setLink({ href: res }).run()
+      }
+    },
+    insertImage: function () {
+      const url = this.fileURL
+      if (url) {
+        this.editor.chain().focus().setImage({ src: url }).run()
+      }
+      this.selectedFile = null
+      this.fileURL = null
+      this.imageModal = false
+    },
+    uploadImage: async function () {
+      const formData = new FormData()
+      formData.append('file', this.selectedFile)
+      await axios({
+        url: '/admin/upload_file',
+        method: 'post',
+        cache: false,
+        data: formData,
+        processData: false,
+        contentType: false
+      }).then(res => {
+        if (!res.data.success) {
+          alert('Upload Image failed')
+        } else if (res.data.file_path.endsWith('.jpg') || res.data.file_path.endsWith('.png')) {
+          this.fileURL = res.data.file_path
+        }
+      })
+    },
+    insertFile: function (link) {
+      this.editor.chain().focus().insertContent(this.fileLink).run()
+    },
+    uploadFile: async function () {
+      const formData = new FormData()
+      formData.append('file', this.selectedFile)
+      await axios({
+        url: '/admin/upload_file',
+        method: 'post',
+        cache: false,
+        data: formData,
+        processData: false,
+        contentType: false
+      }).then(res => {
+        if (!res.data.success) {
+          alert('Upload file failed')
+        } else {
+          const link = '<a target="_blank" href="' + res.data.file_path + '">' + res.data.file_name + '</a>'
+          this.fileLink = link
+        }
+      })
+    }
   }
 }
 </script>
@@ -142,7 +280,6 @@ export default {
 }
 .ProseMirror {
   padding: 1.25rem 1rem;
-  border
 
   > * + * {
     margin-top: 0.75em;
@@ -185,6 +322,10 @@ export default {
   img {
     max-width: 100%;
     height: auto;
+
+    &.ProseMirror-selectednode {
+      outline: 3px solid #68CEF8;
+    }
   }
 
   blockquote {
@@ -196,6 +337,9 @@ export default {
     border: none;
     border-top: 2px solid rgba(#0D0D0D, 0.1);
     margin: 2rem 0;
+    &.ProseMirror-selectednode {
+      outline: 3px solid #68CEF8;
+    }
   }
 }
 </style>
