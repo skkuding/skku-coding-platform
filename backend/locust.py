4e1a1e6c480a58caceb79e4771620c94b87de1e2
joft-ware <zxc91911003@gmail.com>
2021-07-28 23:52:06 +0900

    add locust.py for load test

---
+++ backend/locust.py
@@ -0,0 +1,22 @@
+from locust import HttpUser, task, between
+
+
+class MyLocust(HttpUser):
+    wait_time = between(0, 1)
+    userid = 1
+
+    def on_start(self):
+        self.testcode = 1
+
+    @task
+    def index(self):
+        temp = "root" + str(self.userid)
+        self.userid = (self.userid) % 100 + 1
+        response = self.client.get("/api/profile")
+        self.csrftoken = response.cookies["csrftoken"]
+        response = self.client.post("/api/login", data={"username": temp, "password": "1"}, headers={"X-CSRFToken": self.csrftoken, "Referer": "http://127.0.0.1/"})
+        sessionid = response.cookies["sessionid"]
+        response = self.client.post("/api/submission", data={"problem_id": 1, "language": "C", "code": f"{self.testcode}"},
+                                    headers={"X-CSRFToken": self.csrftoken, "Cookie": f"sessionid={sessionid}; csrftoken={self.csrftoken}", "Referer": "http://127.0.0.1/"})
+        self.testcode += 1
+        self.client.get("/logout")
