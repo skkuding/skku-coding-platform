3d25dfbad07e4ee1f2fb47c15db274c7a2878da6
HakSan <cranemont@gmail.com>
2021-07-31 21:09:17 +0900

    Initialize for lecture API
    Create assignment app -> create assignment model
    Create course app -> create course, takes model
    Add assignment, type field to Problem model
    Add assignment, score field to Submission model

---
+++ backend/course/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from datetime import date
+
+from account.models import User
+
+# Create your models here.
+class Course(models.Model):
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.TextField()
+    course_code = models.TextField()
+    class_number = models.IntegerField()
+    registered_year = models.DateField(default=date.today)
+    semester = models.IntegerField()
+
+
+    class Meta:
+        db_table = "course"
+        ordering = ("-registered_year",)
+
+
+class Takes(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    class Meta:
+        db_table = "takes"
