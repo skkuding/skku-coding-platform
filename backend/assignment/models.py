ec593da78503ddd1b04f59f63518c9a2ea9b5531
HakSan <cranemont@gmail.com>
2021-08-01 19:44:56 +0900

    Update Assignment model

--- backend/assignment/models.py
+++ backend/assignment/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.timezone import now
 
 from course.models import Course
 from account.models import User
@@ -8,12 +9,24 @@ class Assignment(models.Model):
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     title = models.TextField()
-    description = RichTextField()
+    content = RichTextField()
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
     create_time = models.DateTimeField(auto_now_add=True)
     last_update_time = models.DateTimeField(auto_now=True)
     visible = models.BooleanField(default=True)
+
+    @property
+    def status(self):
+        if self.start_time > now():
+            # NOT_START return 1
+            return "1"
+        elif self.end_time < now():
+            # ENDED return -1
+            return "-1"
+        else:
+            # UNDERWAY return 0
+            return "0"
 
     class Meta:
         db_table = "assignment"
