08c1671ab61ca38aa217561566b8b3379dc97fcd
HAKSAN <cranemont@gmail.com>
2021-08-07 19:03:16 +0900

    Add course API
    CouseAPI, StudentManagementAPI

--- backend/course/serializers.py
+++ backend/course/serializers.py
@@ -1,6 +1,6 @@
 from django.db.models import fields
 from utils.api import UsernameSerializer, serializers
-
+from account.serializers import UserAdminSerializer
 from .models import Course, Takes
 
 class CreateCourseSerializer(serializers.Serializer):
@@ -11,29 +11,59 @@ class CreateCourseSerializer(serializers.Serializer):
     semester = serializers.IntegerField()
 
 
-class RegisterStudentSerializer(serializers.Serializer):
+class EditCourseSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=64)
+    course_code = serializers.CharField(max_length=64)
+    class_number = serializers.IntegerField()
+    registered_year = serializers.IntegerField()
+    semester = serializers.IntegerField()
+
+
+class RegisterSerializer(serializers.Serializer):
     user_id = serializers.IntegerField()
     course_id = serializers.IntegerField()
 
 
+class EditRegisterSerializer(serializers.Serializer):
+    registration_id = serializers.IntegerField()
+    course_id = serializers.IntegerField()
+
+
 class CourseSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Course
+        fields = "__all__"
+
+
+class CourseProfessorSerializer(serializers.ModelSerializer):
     created_by = UsernameSerializer()
 
     class Meta:
         model = Course
-        fields = '__all__'
+        fields = "__all__"
 
 
-class CourseListSerializer(serializers.ModelSerializer):
-    data = CourseSerializer(source='course')
+class CourseStudentSerializer(serializers.ModelSerializer):
+    course = CourseSerializer()
 
     class Meta:
         model = Takes
-        fields = ('data',)
+        fields = ('course',)
 
 
 class TakesSerializer(serializers.ModelSerializer):
+    user = UsernameSerializer()
 
     class Meta:
         model = Takes
-        fields = '__all__'
+        fields = "__all__"
+
+
+class UserListSerializer(serializers.ModelSerializer):
+    user = UserAdminSerializer()
+
+    class Meta:
+        model = Takes
+        fields = "__all__"
--- backend/course/views/professor.py
+++ backend/course/views/professor.py
@@ -2,24 +2,33 @@ from django.http import request
 from utils.api import APIView, validate_serializer
 
 from account.models import User
+from account.decorators import ensure_created_by, admin_role_required
 
 from ..models import Course, Takes
-from ..serializers import CourseSerializer, CreateCourseSerializer, CourseListSerializer, RegisterStudentSerializer, TakesSerializer
+from ..serializers import (CourseProfessorSerializer, CreateCourseSerializer, EditCourseSerializer,
+                            RegisterSerializer, EditRegisterSerializer, TakesSerializer, UserListSerializer)
 
 class CourseAPI(APIView):
+    @admin_role_required
     def get(self, request):
         cousre_id = request.GET.get("id")
+
+        if not id:
+            return self.error("Invalid parameter, id is required")
+
         if cousre_id:
             try:
                 course = Course.objects.get(id=cousre_id)
-                return self.success(CourseSerializer(course).data)
+                ensure_created_by(course, request.user)
+                return self.success(CourseProfessorSerializer(course).data)
             except Course.DoesNotExist:
                 return self.error("Course does not exist")
 
         courses = Course.objects.filter(created_by=request.user)
-        return self.success(self.paginate_data(request, courses, CourseSerializer))
+        return self.success(self.paginate_data(request, courses, CourseProfessorSerializer))
 
     @validate_serializer(CreateCourseSerializer)
+    @admin_role_required
     def post(self, request):
         data = request.data
         course = Course.objects.create(title=data["title"],
@@ -28,24 +37,126 @@ class CourseAPI(APIView):
                                         created_by=request.user,
                                         registered_year=data["registered_year"],
                                         semester=data["semester"])
-        return self.success(CourseSerializer(course).data)
+        return self.success(CourseProfessorSerializer(course).data)
 
-
-class StudentManagementAPI(APIView): # params? data? 
-    @validate_serializer(RegisterStudentSerializer)
-    def post(self, request):
+    @validate_serializer(EditCourseSerializer)
+    @admin_role_required
+    def put(self, request):
         data = request.data
-        course_id = data["course_id"]# request.GET.get('course_id')
-        user_id = data["user_id"]# request.GET.get('user_id')
-
         try:
-            Course.objects.get(id=course_id)
-            User.objects.get(id=user_id)
+            course = Course.objects.get(id=data.pop("id"))
+            ensure_created_by(course, request.user)
         except Course.DoesNotExist:
             return self.error("Course does not exist")
+
+        for k, v in data.items():
+            setattr(course, k, v)
+        course.save()
+        return self.success(CourseProfessorSerializer(course).data)
+
+    @admin_role_required
+    def delete(self, request):
+        id = request.GET.get("id")
+        if not id:
+            return self.error("Invalid parameter, id is required")
+        try:
+            course = Course.objects.get(id=id)
+            ensure_created_by(course, request.user)
+        except Course.DoesNotExist:
+            return self.error("Course does not exists")
+
+        course.delete()
+        return self.success()
+
+
+class StudentManagementAPI(APIView):
+    @validate_serializer(RegisterSerializer)
+    @admin_role_required
+    def post(self, request):
+        data = request.data
+        course_id = data["course_id"]
+        user_id = data["user_id"]
+
+        try:
+            User.objects.get(id=user_id)
+            course = Course.objects.get(id=course_id)
+            ensure_created_by(course, request.user)
         except User.DoesNotExist:
             return self.error("User does not exist")
+        except Course.DoesNotExist:
+            return self.error("Course does not exist")
 
-        takes = Takes.objects.create(user_id=user_id,
-                                    course_id=course_id)
+        try:
+            Takes.objects.get(user_id=user_id, course_id=course_id)
+            return self.error("User has been already registered to the course")
+        except Takes.DoesNotExist:
+            takes = Takes.objects.create(user_id=user_id,
+                                        course_id=course_id)
         return self.success(TakesSerializer(takes).data)
+
+    @admin_role_required
+    def get(self, request):
+        course_id = request.GET.get("course_id")
+        get_students_count = request.GET.get("count")
+
+        if not course_id:
+            return self.error("Invalid parameter, course_id is required")
+
+        try: 
+            course = Course.objects.get(id=course_id)
+            ensure_created_by(course, request.user)
+        except Course.DoesNotExist:
+            return self.error("Course does not exist")
+
+        takes = Takes.objects.filter(course_id=course_id)
+
+        # Return number of total registered students
+        if get_students_count:
+            return self.success({ 'total_students': takes.count() })
+
+        return self.success(self.paginate_data(request, takes, UserListSerializer))
+
+    @validate_serializer(EditRegisterSerializer)
+    @admin_role_required
+    def put(self, request):
+        data = request.data
+        course_id = data["course_id"]
+
+        try:
+            takes = Takes.objects.get(id=data.pop("registration_id"))
+        except Takes.DoesNotExist:
+            return self.error("Register information does not exist")
+
+        try:
+            course = Course.objects.get(id=course_id)
+            ensure_created_by(course, request.user)
+            course = Course.objects.get(id=takes.course_id)
+            ensure_created_by(course, request.user)
+        except Course.DoesNotExist:
+            return self.error("Course does not exist")
+
+        try:
+            Takes.objects.get(user_id=takes.user_id, course_id=course_id)
+            return self.error("User has been already registered to the course")
+        except Takes.DoesNotExist:
+            for k, v in data.items():
+                setattr(takes, k, v)
+            takes.save()
+
+        return self.success(TakesSerializer(takes).data)
+
+    @admin_role_required
+    def delete(self, request):
+        id = request.GET.get("registration_id")
+        if not id:
+            return self.error("Invalid parameter, registration_id is required")
+
+        try:
+            takes = Takes.objects.get(id=id)
+            course = Course.objects.get(id=takes.course_id)
+            ensure_created_by(course, request.user)
+        except Takes.DoesNotExist:
+            return self.error("Register information does not exists")
+
+        takes.delete()
+        return self.success()
