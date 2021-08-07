736142771cccd0fab34f8fbc6143e07eabea5f8d
HAKSAN <cranemont@gmail.com>
2021-08-07 16:55:51 +0900

    Add assignment APIs
    Add assignment constants
    
    visible 필드 추가

--- backend/assignment/serializers.py
+++ backend/assignment/serializers.py
@@ -1,13 +1,14 @@
-from django.db.models import fields
 from utils.api import UsernameSerializer, serializers
 
 from .models import Assignment
 from submission.models import Submission
-from problem.serializers import BaseProblemSerializer
-from problem.serializers import ProblemSerializer
+from course.serializers import CourseSerializer
+
 class AssginmentProfessorSerializer(serializers.ModelSerializer):
     created_by = UsernameSerializer()
+    course = CourseSerializer()
     status = serializers.CharField()
+
     class Meta:
         model = Assignment
         fields = "__all__"
@@ -16,22 +17,38 @@ class AssignmentSerializer(serializers.ModelSerializer):
     status = serializers.CharField()
     total_problem = serializers.SerializerMethodField(read_only=True)
     submitted_problem = serializers.SerializerMethodField(read_only=True)
+    accepted_problem = serializers.SerializerMethodField(read_only=True)
 
     def get_total_problem(self, obj):
         return obj.problems.count()
 
     def get_submitted_problem(self, obj):
-        return Submission.objects.filter(assignment=obj, user_id=obj.created_by).count()
-        # return obj.submissions.filter(user_id=obj.creatd_by).count()
+        user = self.context['request'].user
+        return Submission.objects.filter(assignment=obj, user_id=user.id).values("problem").distinct().count()
+
+    def get_accepted_problem(self, obj):
+        user = self.context["request"].user
+        return Submission.objects.filter(assignment=obj, user_id=user.id, result=0).values("problem").distinct().count()
 
     class Meta:
         model = Assignment
-        fields = "__all__"
+        exclude = ("created_by", "visible", "create_time", "last_update_time", "course")
 
 
 class CreateAssignmentSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=128)
-    course = serializers.IntegerField()
-    content = serializers.CharField()
+    course_id = serializers.IntegerField()
+    content = serializers.CharField(max_length=1024 * 1024 * 8)
     start_time = serializers.DateTimeField()
     end_time = serializers.DateTimeField()
+    visible = serializers.BooleanField()
+
+
+class EditAssignmentSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=128)
+    course_id = serializers.IntegerField()
+    content = serializers.CharField(max_length=1024 * 1024 * 8)
+    start_time = serializers.DateTimeField()
+    end_time = serializers.DateTimeField()
+    visible = serializers.BooleanField()
