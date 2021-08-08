5e3d4061af0f762013a3133453323271cd7d3ed3
HAKSAN <cranemont@gmail.com>
2021-08-08 17:21:43 +0900

    Add assignment submission API

--- backend/submission/urls.py
+++ backend/submission/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import SubmissionAPI, SubmissionListAPI, ContestSubmissionListAPI, SubmissionExistsAPI
+from .views import SubmissionAPI, SubmissionListAPI, ContestSubmissionListAPI, AssignmentSubmissionListAPI, SubmissionExistsAPI
 
 urlpatterns = [
     path("submission/", SubmissionAPI.as_view(), name="submission_api"),
     path("submissions/", SubmissionListAPI.as_view(), name="submission_list_api"),
     path("submission_exists/", SubmissionExistsAPI.as_view(), name="submission_exists"),
     path("contest_submissions/", ContestSubmissionListAPI.as_view(), name="contest_submission_list_api"),
+    path("assignment_submissions/", AssignmentSubmissionListAPI.as_view(), name="assignment_submission_list_api"),
 ]
