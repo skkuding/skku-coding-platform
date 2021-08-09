815214c2feb2b111f2027ae229f67f80aac99e57
HAKSAN <cranemont@gmail.com>
2021-08-09 20:32:00 +0900

    Add swagger
    Remove drf_sepctacular from settings.py
    
    Fix ProblemTagAPI swagger schema responses

--- backend/course/views/student.py
+++ backend/course/views/student.py
@@ -1,10 +1,38 @@
 from utils.api import APIView
 from account.decorators import login_required
-
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 from ..models import Course, Registration
 from ..serializers import CourseSerializer, CourseStudentSerializer
 
 class CourseAPI(APIView):
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter(
+                name="id",
+                in_=openapi.IN_QUERY,
+                description="Unique ID of a course",
+                requied=True,
+                type=openapi.TYPE_INTEGER,
+            ),
+            openapi.Parameter(
+                name="limit",
+                in_=openapi.IN_QUERY,
+                description="Number of courses to show",
+                type=openapi.TYPE_STRING,
+                default=10,
+            ),
+            openapi.Parameter(
+                name="offset",
+                in_=openapi.IN_QUERY,
+                description="ID of the first course of list",
+                type=openapi.TYPE_STRING,
+                default=0,
+            ),
+        ],
+        operation_description="Get registered course list of requesting user",
+        responses={200: CourseStudentSerializer},
+    )
     @login_required
     def get(self, request):
         course_id = request.GET.get("id")
@@ -15,9 +43,9 @@ class CourseAPI(APIView):
 
         if course_id:
             try:
-                course = Course.objects.get(id=course_id)
-                Registration.objects.get(user_id=user_id, course_id=course_id)
-                return self.success(CourseSerializer(course).data)
+                Course.objects.get(id=course_id)
+                course = Registration.objects.get(user_id=user_id, course_id=course_id)
+                return self.success(CourseStudentSerializer(course).data)
             except Course.DoesNotExist:
                 return self.error("Course does not exist")
             except Registration.DoesNotExist:
