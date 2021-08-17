049bf0994bfc5232896677f9d5a445d1a485c168
ghdwlsrl3229 <ghdwlsrl3229@naver.com>
2021-08-17 13:26:55 +0900

    webpush wp_send_message의 로깅 제거
    해당 함수의 로직은 device가 만료되었거나 유효하지 않으면 예외를 일으켜 해당 device를 제거하도록 되어 있음.
    로깅을 적용했을 때 불필요한 로그가 너무 빈번하게 찍히는 것 같아서 해당 함수의 로깅 기능을 제거함.

--- backend/utils/webpush.py
+++ backend/utils/webpush.py
@@ -1,11 +1,6 @@
-import logging
-
-logger = logging.getLogger(__name__)
-
 def wp_send_message(devices, message):
     for device in devices:
         try:
             device.send_message(message)
         except Exception as e:
             device.delete()
-            logger.exception(e)
