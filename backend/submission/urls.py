b5a310c09168d746437b15d3f9f917063de9521f
HAKSAN <cranemont@gmail.com>
2021-08-20 18:30:08 +0900

    Add EditSubmissionScoreAPI

--- backend/submission/urls.py
+++ backend/submission/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from .views import SubmissionAPI, SubmissionListAPI, ContestSubmissionListAPI, AssignmentSubmissionListAPI, AssignmentSubmissionListProfessorAPI, SubmissionExistsAPI
+from .views import SubmissionAPI, SubmissionListAPI, ContestSubmissionListAPI, AssignmentSubmissionListAPI, AssignmentSubmissionListProfessorAPI, SubmissionExistsAPI, EditSubmissionScoreAPI
 
 urlpatterns = [
     url(r"^submission/?$", SubmissionAPI.as_view(), name="submission_api"),
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r"^contest_submissions/?$", ContestSubmissionListAPI.as_view(), name="contest_submission_list_api"),
     url(r"^assignment_submissions/?$", AssignmentSubmissionListAPI.as_view(), name="assignment_submission_list_api"),
     url(r"^assignment_submissions_professor/?$", AssignmentSubmissionListProfessorAPI.as_view(), name="assignment_submission_list_professor_api"),
+    url(r"^edit_submission_score/?$", EditSubmissionScoreAPI.as_view(), name="edit_submission_score_api"),
 ]
