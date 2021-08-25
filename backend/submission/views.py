502e15c4f4b345d4b21dc9bd6286eba4bce76a1e
minseo999 <alstj778@gmail.com>
2021-08-25 22:14:02 +0900

    Fix SubmissionListAPI, ContestSubmissionListAPI all submissions에서 일반 유저에게 admin 계정 제출 목록 보이지 않도록 API 수정

--- backend/submission/views.py
+++ backend/submission/views.py
@@ -9,6 +9,7 @@ from judge.tasks import judge_task
 from options.options import SysOptions
 # from judge.dispatcher import JudgeDispatcher
 from problem.models import Problem, ProblemRuleType
+from account.models import AdminType, User
 from utils.api import APIView, validate_serializer
 from utils.cache import cache
 from utils.captcha import Captcha
@@ -194,8 +195,11 @@ class SubmissionListAPI(APIView):
             submissions = submissions.filter(problem=problem)
         if (myself and myself == "1") or not SysOptions.submission_list_show_all:
             submissions = submissions.filter(user_id=request.user.id)
-        elif username:
-            submissions = submissions.filter(username__icontains=username)
+        elif (myself and myself == "0") and not request.user.is_admin_role():
+            user_ids = submissions.values_list("user_id", flat=True)
+            users = User.objects.filter(id__in=user_ids, admin_type=AdminType.REGULAR_USER)
+            user_ids = users.values_list("id")
+            submissions = submissions.filter(user_id__in=user_ids)
         if result:
             submissions = submissions.filter(result=result)
         data = self.paginate_data(request, submissions)
@@ -268,8 +272,11 @@ class ContestSubmissionListAPI(APIView):
 
         if myself and myself == "1":
             submissions = submissions.filter(user_id=request.user.id)
-        elif username:
-            submissions = submissions.filter(username__icontains=username)
+        elif (myself and myself == "0") and not request.user.is_admin_role():
+            user_ids = submissions.values_list("user_id", flat=True)
+            users = User.objects.filter(id__in=user_ids, admin_type=AdminType.REGULAR_USER)
+            user_ids = users.values_list("id")
+            submissions = submissions.filter(user_id__in=user_ids)
         if result:
             submissions = submissions.filter(result=result)
 
