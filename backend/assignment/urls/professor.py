5f11ab03db6965a09beae7389fb1fba20abc2f55
HAKSAN <cranemont@gmail.com>
2021-08-28 16:38:48 +0900

    Add download assignment submission API

--- backend/assignment/urls/professor.py
+++ backend/assignment/urls/professor.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from ..views.professor import AssignmentAPI
+from ..views.professor import AssignmentAPI, DownloadAssignmentSubmissions
 
 urlpatterns = [
     url(r"^course/assignment/?$", AssignmentAPI.as_view(), name="assignment_professor_api"),
+    url(r"^download_submissions/?$", DownloadAssignmentSubmissions.as_view(), name="download_assignment_submissions"),
 ]
