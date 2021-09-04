4b030d023f7bddffc3d8431ccbd031f46002cf2f
HAKSAN <cranemont@gmail.com>
2021-09-04 23:44:21 +0900

    Add accepted problem count to AssignmentSerializer

--- backend/assignment/serializers.py
+++ backend/assignment/serializers.py
@@ -19,6 +19,7 @@ class AssignmentSerializer(serializers.ModelSerializer):
     status = serializers.CharField()
     total_problem = serializers.SerializerMethodField(read_only=True)
     submitted_problem = serializers.SerializerMethodField(read_only=True)
+    accepted_problem = serializers.SerializerMethodField(read_only=True)
 
     def get_total_problem(self, obj):
         return obj.problems.count()
@@ -26,6 +27,10 @@ class AssignmentSerializer(serializers.ModelSerializer):
     def get_submitted_problem(self, obj):
         user = self.context["request"].user
         return Submission.objects.filter(assignment=obj, user_id=user.id).values("problem").distinct().count()
+
+    def get_accepted_problem(self, obj):
+        user = self.context["request"].user
+        return Submission.objects.filter(assignment=obj, user_id=user.id, result=0).values("problem").distinct().count()
 
     class Meta:
         model = Assignment
