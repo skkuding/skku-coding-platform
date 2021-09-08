1b53fc8f05b6f2d732964c352aab717381a2c101
HakSan <cranemont@gmail.com>
2021-09-08 23:36:33 +0900

    Add problem rule type ASSIGNMENT
    OI type에서 테스트케이스 점수 설정 가능 및 개별 점수의 총합으로 score 저장하게 구현됨
    ASSIGNMENT rule type을 추가해 OI type과 동일하게 사용, 나중에 부가 기능 필요시 따로 구현

--- backend/problem/serializers.py
+++ backend/problem/serializers.py
@@ -58,7 +58,7 @@ class CreateOrEditProblemSerializer(serializers.Serializer):
     memory_limit = serializers.IntegerField(min_value=1, max_value=1024)
     languages = LanguageNameMultiChoiceField()
     template = serializers.DictField(child=serializers.CharField(min_length=1))
-    rule_type = serializers.ChoiceField(choices=[ProblemRuleType.ACM, ProblemRuleType.OI])
+    rule_type = serializers.ChoiceField(choices=[ProblemRuleType.ACM, ProblemRuleType.OI, ProblemRuleType.ASSIGNMENT])
     io_mode = ProblemIOModeSerializer()
     spj = serializers.BooleanField()
     spj_language = SPJLanguageNameChoiceField(allow_blank=True, allow_null=True)
