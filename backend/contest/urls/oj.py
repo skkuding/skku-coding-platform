d461d8de1022285cdcdc3d26b0fbcaa9d78ca888
cyw320712 <cyw7515@naver.com>
2021-10-16 23:02:24 +0900

    Add rankapi url

--- backend/contest/urls/oj.py
+++ backend/contest/urls/oj.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from ..views.oj import ContestAnnouncementListAPI
 from ..views.oj import ContestPasswordVerifyAPI, ContestAccessAPI
-from ..views.oj import ContestListAPI, ContestAPI
+from ..views.oj import ContestListAPI, ContestAPI, ContestRankAPI
 
 urlpatterns = [
     path("contests/", ContestListAPI.as_view(), name="contest_list_api"),
@@ -10,4 +10,5 @@ urlpatterns = [
     path("contest/password/", ContestPasswordVerifyAPI.as_view(), name="contest_password_api"),
     path("contest/announcement/", ContestAnnouncementListAPI.as_view(), name="contest_announcement_api"),
     path("contest/access/", ContestAccessAPI.as_view(), name="contest_access_api"),
+    path("contest/rank/", ContestRankAPI.as_view(), name="contest_rank_api"),
 ]
