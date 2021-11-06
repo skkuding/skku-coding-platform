9892f8af6c541ba52f457074ed86bf32009f84a0
Jaemin Choi <1dotolee@gmail.com>
2021-11-06 13:39:44 +0000

    fix: Use host of db for each environment
    GitHub Actions와 개발 환경(vscode devcontainer)에서 DB Container가 동작하는
    방식이 다르므로 별도의 hostname을 지정한다.

--- backend/oj/dev_settings.py
+++ backend/oj/dev_settings.py
@@ -1,13 +1,13 @@
-# coding=utf-8
 import os
+from utils.shortcuts import get_env
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': 'db',
-        'PORT': 5432,
+        'HOST': get_env('POSTGRES_HOST', '127.0.0.1'),
+        'PORT': get_env('POSTGRES_PORT', '5432'),
         'NAME': "codingplatform",
         'USER': "codingplatform",
         'PASSWORD': 'codingplatform'
@@ -15,8 +15,8 @@ DATABASES = {
 }
 
 REDIS_CONF = {
-    "host": "redis",
-    "port": "6379"
+    "host": get_env('REDIS_HOST', "127.0.0.1"),
+    "port": get_env('REDIS_PORT', "6379")
 }
 
 
