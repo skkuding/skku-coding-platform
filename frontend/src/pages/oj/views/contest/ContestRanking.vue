695a0e0b98f94cea0ae0fa545cf2bfa36fa57d4e
SOLBI1028 <hagkthfql@naver.com>
2021-11-09 00:37:53 +0900

    Implement ranking page

--- frontend/src/pages/oj/views/contest/ContestRanking.vue
+++ frontend/src/pages/oj/views/contest/ContestRanking.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="contest-ranking-card font-bold">
     <div class="top-bar mb-4">
-      <!-- <h2 class="title"> {{ contest.title + 'Standings' }} </h2> -->
       <h2 class="title"> Standings </h2>
       <b-badge
         class="time-limit-container"
@@ -17,32 +16,21 @@
         :fields="contestRankingListFields"
         head-variant="light"
       >
-        <template #cell(A)="data">
-          <row class="userscore">{{ data.value.score }}</row>
-          <row class="usertime"> {{ data.value.time }} </row>
+        <template v-for="problem in contestProblemID"
+          v-slot:[`cell(${problem})`]="data"
+        >
+          <div v-if="data.item[problem].is_ac === true" :key="problem">
+            <div>{{ data.item[problem].score }}</div>
+            <div>{{ data.item[problem].ac_time }}</div>
+          </div>
+          <div v-else-if="data.item[problem].problem_submission === '0'" :key="problem"> {{ '-' }}</div>
+          <div v-else :key="problem" class="ac-false"> {{ '-' + data.item[problem].problem_submission }} </div>
         </template>
-        <template #cell(B)="data">
-          <row class="userscore"> {{ data.value.score }} </row>
-          <row class="usertime"> {{ data.value.time }} </row>
-        </template>
-        <template #cell(C)="data">
-          <row class="userscore"> {{ data.value.score }} </row>
-          <row class="usertime"> {{ data.value.time }} </row>
-        </template>
-        <template #cell(AC)="data">
+        <template #cell(accepted_number)="data">
           <div class="userAC">{{ data.value }}</div>
         </template>
-        <template #cell(totalscore)="data">
+        <template #cell(total_score)="data">
           <div class="userTotalscore">{{ data.value }}</div>
-        </template>
-        <template #thead-top="">
-          <b-tr>
-            <b-th colspan="2"><span class="sr-only">Ranking and Username</span></b-th>
-            <b-th>A</b-th>
-            <b-th>B</b-th>
-            <b-th>C</b-th>
-            <b-th centered colspan="2">Total</b-th>
-          </b-tr>
         </template>
       </b-table>
     </div>
@@ -65,62 +53,75 @@ export default {
   data () {
     return {
       contestID: '',
-      contestRankingList: [
-        {
-          rank: 1,
-          user_name: '2018310000',
-          A: { totalscore: 500, score: 300, time: '00:02' },
-          B: { totalscore: 500, score: 700, time: '00:30' },
-          C: { totalscore: 500, score: 1500, time: '01:50' },
-          AC: 3,
-          totalscore: 2500
-        },
-        {
-          rank: 2,
-          user_name: '2018311111',
-          A: { totalscore: 500, score: 300, time: '00:02' },
-          B: { totalscore: 500, score: 600, time: '00:30' },
-          C: { totalscore: 500, score: 0, time: '0' },
-          AC: 2,
-          totalscore: 900
-        },
-        {
-          rank: 3,
-          user_name: '2018312222',
-          A: { totalscore: 500, score: 200, time: '00:02' },
-          B: { totalscore: 500, score: 0, time: '0' },
-          C: { totalscore: 500, score: 0, time: '0' },
-          AC: 1,
-          totalscore: 200
-        }
-      ],
+      contestRankingList: [],
       contestRankingListFields: [
         {
-          key: 'rank',
+          key: 'id',
           label: '#'
         },
         {
-          key: 'user_name',
+          key: 'user.username',
           label: 'User',
           formatter: value => {
             return value.substr(0, 4) + '****' + value.substr(8, 2)
           }
-        }, 'A', 'B', 'C', 'AC', 'totalscore'
-      ]
+        }
+      ],
+      contest: {},
+      contestProblems: [],
+      contestProblemID: []
     }
   },
   async mounted () {
     this.contestID = this.$route.params.contestID
+    await this.getContestDetail()
+    await this.getContestProblems()
+    for (const contestProblem of this.contestProblems) {
+      this.contestProblemID.push((contestProblem.id) + '')
+      this.contestRankingListFields.push({ key: contestProblem.id + '', label: contestProblem._id }) // table 에 표시되는건 display id
+    }
+    this.contestRankingListFields.push({ key: 'accepted_number', label: 'AC' })
+    this.contestRankingListFields.push({ key: 'total_score', label: 'Total Score' })
     await this.getContestRanking()
+    this.contestRankingList.problem = []
+    this.setRankingList()
+    console.log(this.contestRankingList)
   },
   methods: {
+    async getContestDetail () {
+      try {
+        const res = await this.$store.dispatch('getContest')
+        const data = res.data.data
+        this.contest = data
+        console.log(data)
+      } catch (err) {
+      }
+    },
+    async getContestProblems () {
+      try {
+        const res = await this.$store.dispatch('getContestProblems')
+        const data = res.data.data
+        this.contestProblems = data
+      } catch (err) {
+      }
+    },
     async getContestRanking () {
       try {
         const res = await api.getContestRanking(this.contestID, 1)
-        console.log(res)
+        const data = res.data.data.results
+        this.contestRankingList = data
       } catch (err) {
-        console.log('error')
-        console.log(err)
+      }
+    },
+    setRankingList () {
+      for (const rank of this.contestRankingList) {
+        for (const problem of this.contestProblemID) {
+          if (Object.keys(rank.submission_info).includes(problem) === true) {
+            this.$set(rank, problem, rank.submission_info[problem])
+          } else {
+            this.$set(rank, problem, { problem_submission: '0' })
+          }
+        }
       }
     }
   },
@@ -163,6 +164,12 @@ export default {
   .table{
     width: 95% !important;
     margin: 0 auto;
+  }
+  .table-head{
+    text-align: center;
+  }
+  .ac-false {
+    color: #E9A05A;
   }
   .userscore{
     color: #8DC63F;
