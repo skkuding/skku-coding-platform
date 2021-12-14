7578cd34636d1d78fcfa61e41eca9ff7ec521645
Jaemin Choi <1dotolee@gmail.com>
2021-12-14 10:02:48 +0000

    Apply fie based routing
    `vite-plugin-pages`를 이용하여 route를 쉽게 생성한다.
    
    https://github.com/hannoeru/vite-plugin-pages

--- frontend-new/src/main.ts
+++ frontend-new/src/main.ts
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
+// eslint-disable-next-line import/no-unresolved
+import routes from 'virtual:generated-pages'
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+const app = createApp(App)
+
+app.use(router)
+app.mount('#app')
