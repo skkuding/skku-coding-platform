c422065db027f1147ecea3a155133b92c19fbfbd
Jaemin Choi <1dotolee@gmail.com>
2021-12-14 12:13:49 +0000

    Apply auto importing components
    `unplugin-vue-components`을 이용해 별도의 `import` 문 없이 자동으로 component를 불러온다.
    
    https://github.com/antfu/unplugin-vue-components

--- frontend-new/vite.config.ts
+++ frontend-new/vite.config.ts
@@ -3,10 +3,18 @@ import path from 'path'
 import Vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
 import Layouts from 'vite-plugin-vue-layouts'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [Vue(), Pages(), Layouts()],
+  plugins: [
+    Vue(),
+    Pages(),
+    Layouts(),
+    Components({
+      dts: 'src/components.d.ts'
+    })
+  ],
   resolve: {
     alias: {
       '~': path.resolve(__dirname, './src')
