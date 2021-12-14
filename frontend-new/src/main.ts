20e0a3c6f23ba29cdddc6f47ee5f22fe3fe6cf24
Jaemin Choi <1dotolee@gmail.com>
2021-12-14 11:05:35 +0000

    Use Tailwind CSS
    CSS Framework로 Tailwind CSS를 사용한다.

--- frontend-new/src/main.ts
+++ frontend-new/src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 import routes from 'virtual:generated-pages'
+import './index.css'
 
 const router = createRouter({
   history: createWebHistory(),
