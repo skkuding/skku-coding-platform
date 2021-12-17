13774477d47814f8716f92bde5f80ab8d44159d8
Jaemin Choi <1dotolee@gmail.com>
2021-12-17 10:40:29 +0000

    Use Manrope and Noto Sans KR as default font

--- frontend-new/tailwind.config.js
+++ frontend-new/tailwind.config.js
@@ -1,10 +1,21 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     './index.html',
     './src/**/*.{vue,js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: [
+          'Manrope',
+          'Noto Sans KR',
+          ...defaultTheme.fontFamily.sans
+        ]
+      }
+    }
   },
   plugins: []
 }
