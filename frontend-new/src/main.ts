24d1bca1ec844f696e0d190cb313d87839b121de
Jaemin Choi <1dotolee@gmail.com>
2021-12-19 14:42:28 +0000

    Use simple progress bar
    NProgress로 상단에 로딩 바를 보여준다.
    
    https://github.com/rstacruz/nprogress

--- frontend-new/src/main.ts
+++ frontend-new/src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import generatedRoutes from 'virtual:generated-pages'
 import { setupLayouts } from 'virtual:generated-layouts'
+import NProgress from 'nprogress'
 import App from './App.vue'
 import './index.css'
 
@@ -11,6 +12,9 @@ const router = createRouter({
   history: createWebHistory(),
   routes
 })
+
+router.beforeEach(() => { NProgress.start() })
+router.afterEach(() => { NProgress.done() })
 
 const app = createApp(App)
 
