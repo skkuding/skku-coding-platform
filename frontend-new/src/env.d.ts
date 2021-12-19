3252fd36c2154ba852aee996acbe6a6a9f3ff38c
Jaemin Choi <1dotolee@gmail.com>
2021-12-19 14:46:39 +0000

    Specify type sources in tsconfig
    `*.d.ts` 파일에 package type을 적는 대신, tsconfig에 명시한다.
    더불어, `unplugin-icons`의 type을 추가한다.

--- frontend-new/src/env.d.ts
+++ frontend-new/src/env.d.ts
@@ -1,7 +1,3 @@
-/// <reference types="vite/client" />
-/// <reference types="vite-plugin-pages/client" />
-/// <reference types="vite-plugin-vue-layouts/client" />
-
 declare module '*.vue' {
   import { DefineComponent } from 'vue'
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
