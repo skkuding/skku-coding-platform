98aeec397e26ef0a38c9f94208e13e98f5632708
minseo999 <alstj778@gmail.com>
2021-12-26 16:36:15 +0900

    [Student] Add bookmark course API - add bookmark field to Registration model - edit CourseAPI for bookmark course list

--- backend/course/migrations/0003_auto_20211226_1458.py
+++ backend/course/migrations/0003_auto_20211226_1458.py
@@ -6,12 +6,17 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('conf', '0004_auto_20180501_0436'),
+        ('course', '0002_auto_20210808_1709'),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='judgeserver',
+            model_name='course',
+            name='id',
+            field=models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
+        ),
+        migrations.AlterField(
+            model_name='registration',
             name='id',
             field=models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
         ),
