29db9e8a75956cf2bbcd6dfa057fdc1c8b209cd5
Jimin Ha <jimin9038@g.skku.edu>
2021-12-30 01:28:13 +0900

    Implement CLI Frontend
    - Xterm.js 라이브러리를 사용하여 CLI 프론트엔드 구현 예정
    - 기존의 socket으로 구현된 것을 활용하여 stdin, stdout, stderr를 하나의
    에디터(터미널)에 보이도록 함
    - run 한 후에 입력 가능
    - compile은 run버튼을 눌렀을 때 같이 진행되도록 하였음.

--- frontend/src/pages/oj/views/CLI.vue
+++ frontend/src/pages/oj/views/CLI.vue
@@ -10,24 +10,28 @@
     </select>
     <form>
       <p><textarea cols="60" rows="8" v-model="code"></textarea></p>
-      <button type="button" value="Connect" @click="compile()">compile</button>
+      <!-- <button type="button" value="Connect" @click="compile()">compile</button> -->
       <button type="button" value="Connect" @click="run()">Run</button>
     </form>
+    <div style = "margin-left: 30px; margin-right:30px" ref="terminal"/>
     <h1> stdin </h1>
     <form>
-      <input type="text" v-model="stdin">
-      <button type="button" value="input" @click="postStdin()">input</button>
+      <input type="text" v-model="stdin" readonly>
+      <!-- <button type="button" value="input" @click="postStdin()">input</button> -->
     </form>
     <h1> stdout </h1>
-    <p><textarea cols="40" rows="6" v-model="stdout"></textarea></p>
-    <h1> stderr </h1>
-    <p>{{ stderr }}</p>
+    <p><textarea cols="40" rows="6" v-model="stdout" readonly></textarea></p>
   </div>
 </template>
 
 <script>
 import io from 'socket.io-client'
 import axios from 'axios'
+import { Terminal } from 'xterm'
+
+import 'xterm/lib/addons/fullscreen/fullscreen.css'
+import 'xterm/dist/xterm.css'
+
 export default {
   name: 'socket',
   data () {
@@ -36,22 +40,99 @@ export default {
       stdout: '',
       stderr: '',
       socket: '',
-      code: '',
+      code: 'inp = input()\nprint(inp, inp)',
       compileResult: '',
       dir: null,
       lang: '',
       lastStdin: '',
-      userinput: false
+      userinput: false,
+      writable: false
+    }
+  },
+  mounted () {
+    this.term = new Terminal({
+      rendererType: 'canvas',
+      rows: 25,
+      cols: 100,
+      convertEol: true,
+      scrollback: 50,
+      disableStdin: false,
+      cursorBlink: true,
+      cursorStyle: 'underline',
+      theme: {
+        foreground: '#7e9192',
+        background: '#002833',
+        cursor: 'help',
+        lineHeight: 16
+      }
+    })
+    this.term.prompt = () => {
+      this.term.write('\r< ')
+    }
+    this.term.writeln(
+      `
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+▓▓▓▓▓▓▒▒      ░▒▓▓▓▓
+▓▓▓▓▒ ▒░░   ░▒▓░ ░ ▓
+▓▓▒  ▒░░░ ░▒ ░▓▓▓▓▓▓
+▓▒ ▒░░   ░░▓ ▒▓▓▓▓▓▓
+▓░ ░░░  ░░░▒   ▓▓▓▓▓
+▓  ░   ░░░▒  ▒ ▒▓▓▓▓
+▓▓ ▒ ░▒░▒▓▓▓▓▓ ░ ▒▓▓
+▓▓░ ▒ ▓▓▓▓▓▓ ▓▓▓ ░▓▓
+▓▓▓▒ ▒▓▓▓ ▓▓▓▓▓  ▓▓▓
+▓▓▓▓▓▓▓░░▒▓▓▓▓▒ ▒▓▓▓
+▓▓▓▓▓▓▓▓▓▒▒▒▒▒▓▓▓▓▓▓
+      `
+    )
+    this.term.writeln('Welcome to \x1B[1;32mSKKU Coding platform!\x1B[0m')
+    this.term.writeln('Please Run your code!!')
+    this.term.open(this.$refs.terminal)
+    this.term.onKey(e => {
+      if (!this.writable) {
+        return
+      }
+      const printable = !e.domEvent.altKey && !e.domEvent.altGraphKey && !e.domEvent.ctrlKey && !e.domEvent.metaKey
+      if (e.domEvent.keyCode === 13) {
+        this.term.write('\n')
+        this.socket.emit('stdin', this.stdin)
+        this.userinput = true
+      } else if (e.domEvent.keyCode === 8) {
+        if (this.term._core.buffer.x > 2) {
+          this.term.write('\b \b')
+          this.stdin = this.stdin.length === 1 ? '' : this.stdin.substring(0, this.stdin.length - 1)
+        }
+      } else if (printable) {
+        this.term.write(e.key)
+        this.stdin += e.key
+      } else {
+        this.stdin += e.key
+      }
+    })
+  },
+  beforeDestroy () {
+    if (this.term) {
+      this.term.dispose()
     }
   },
   methods: {
-    run () {
+    async run () {
+      const ack = await this.compile()
+
       if (this.dir === null) {
         console.log('Not compiled yet')
         return
       }
+      if (ack === -1) {
+        return
+      }
+      this.term.clear()
+      this.term.writeln('\x1B[1;3;31mCode running...\x1B[0m')
+      this.term.prompt()
+      this.stdin = ''
       this.stdout = ''
       this.stderr = ''
+
       this.socket = io('http://localhost:40000', {
         reconnection: false,
         query: {
@@ -59,16 +140,23 @@ export default {
           lang: this.lang
         }
       })
+
+      this.writable = true
+
       this.socket.on('stdout', (output) => {
         if (this.userinput) {
-          this.stdout += output.replace(this.stdin, '')
+          this.stdout = output.replace(this.stdin + '\r\n', '')
+          this.term.write(this.stdout)
           this.userinput = false
         } else {
           this.stdout += output
+          this.term.write(this.stdout)
         }
       })
+
       this.socket.on('exited', () => {
-        console.log('program exited')
+        this.term.writeln('\x1B[1;3;31mProgram exited\x1B[0m')
+        this.writable = false
       })
     },
     postStdin () {
@@ -82,8 +170,11 @@ export default {
         this.stderr = ''
         if (res.data.status !== 1) {
           this.stderr = res.data.output
+          this.term.writeln('\x1B[1;31m' + res.data.output + '\x1B[0m')
+          return -1
         } else {
           this.dir = res.data.output
+          return 0
         }
       } catch (err) {
         console.log(err)
