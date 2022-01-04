1708eeef07e81302ea3839fbf07d90e55525e44c
y-jiu <chldudwn99@gmail.com>
2022-01-04 23:34:45 +0900

    Make url for question

--- backend/course/urls/student.py
+++ backend/course/urls/student.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from ..views.student import CourseAPI
+from ..views.student import CourseAPI, QuestionAPI
 
 urlpatterns = [
     url(r"^course/?$", CourseAPI.as_view(), name="course_api"),
+    url(r"^question/?$", QuestionAPI.as_view(), name="question_api"),
 ]
