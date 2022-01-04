5a73c9b088b8008f4ba60803b5ad7211eac5eea7
y-jiu <chldudwn99@gmail.com>
2022-01-04 23:31:47 +0900

    Make model for question

--- backend/course/models.py
+++ backend/course/models.py
@@ -22,3 +22,16 @@ class Registration(models.Model):
 
     class Meta:
         db_table = "registration"
+
+class Question(models.Model):
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
+    title = models.TextField()
+    content = models.TextField()
+    create_time = models.DateTimeField()
+    last_update_time = models.DateTimeField()
+    status = models.BooleanField()
+
+    class Meta:
+        db_table = "question"
+        ordering = ("-create_time",)
