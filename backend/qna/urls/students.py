8e93288da33aabc956e1d9a6abdab227410b3b7c
y-jiu <chldudwn99@gmail.com>
2022-01-07 04:02:51 +0900

    Add Question url

--- backend/qna/urls/students.py
+++ backend/qna/urls/students.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from ..views import AnswerAPI
+from ..views import AnswerAPI, QuestionAPI
 
 urlpatterns = [
+    path("course/question", QuestionAPI.as_view(), name="question_api"),
     path("course/question/answer", AnswerAPI.as_view(), name="answer_api")
 ]
