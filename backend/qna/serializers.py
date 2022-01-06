ec09a1f1b425504fc7ec01a3d971c865be2a5dfb
y-jiu <chldudwn99@gmail.com>
2022-01-07 03:57:11 +0900

    Add serializers

--- backend/qna/serializers.py
+++ backend/qna/serializers.py
@@ -1,6 +1,6 @@
-from utils.api import serializers
+from utils.api import UsernameSerializer, serializers
 from .models import Question, Answer
-
+from course.serializers import CourseSerializer
 
 class AnswerSerializer(serializers.ModelSerializer):
     name = serializers.CharField()
@@ -15,3 +15,22 @@ class CreateAnswerSerializer(serializers.ModelSerializer):
 class EditAnswerSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField()
     content = serializers.CharField()
+
+class QuestionSerializer(serializers.ModelSerializer):
+    created_by = UsernameSerializer()
+    course = CourseSerializer()
+    class Meta:
+        model = Question
+        fields = "__all__"
+
+class CreateQuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+      model = Question
+      fields = "__all__"
+
+
+class EditQuestionSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField()
+    class Meta:
+      model = Question
+      fields = ("title", "content","last_update_time")
