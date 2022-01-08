8e7f8adeb965210938fdb07a8553e04800a4cf85
aintbe <drwho@g.skku.edu>
2022-01-08 20:12:40 +0900

    Start QnA app

---
+++ backend/qna/tests.py
@@ -0,0 +1,20 @@
+from utils.api.tests import APITestCase
+from .models import Question, Answer
+
+#contest test 참조하기
+class AnswerAPITest(APITestCase):
+    def setUp(self):
+        #question 만들기가 선행되어야 해서 아직 작성하지 않았습니다!
+        pass
+    
+    def test_create_answer(self):
+        pass
+    
+    def test_edit_answer(self):
+        pass
+
+    def test_list_answer(self):
+        pass
+
+    def test_delete_answer(self):
+        pass
