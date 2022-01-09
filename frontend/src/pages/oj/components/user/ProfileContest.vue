d4ade56a3e9704c70d2fb43d46c514c7894a389f
Jimin Ha <jimin9038@g.skku.edu>
2022-01-09 03:39:21 +0000

    feat: initialize user profile page
    - User profile 페이지의 공통 부분과 탭을 만든다.

---
+++ frontend/src/pages/oj/components/user/ProfileContest.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="profile-contest">
+    <div class="section-title mt-4 mb-3">
+      My Contest Rank Transition
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ProfileContest',
+  components: {
+  },
+  props: {
+  },
+  data () {
+    return {
+    }
+  },
+  async mounted () {
+  },
+  methods: {
+
+  },
+  computed: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .profile-contest {
+    display: flex;
+    flex-direction: column;
+    align-items: center
+  }
+
+</style>
