3ed046fb4bac853a9fb58a55d5ebbfcccd3983fd
aintbe <drwho@g.skku.edu>
2022-01-10 21:27:06 +0900

    Fix minor issues

--- backend/qna/serializers.py
+++ backend/qna/serializers.py
@@ -6,33 +6,37 @@ from course.serializers import CourseSerializer
 class QuestionSerializer(serializers.ModelSerializer):
     created_by = UsernameSerializer()
     course = CourseSerializer()
+
     class Meta:
         model = Question
         fields = "__all__"
 
-class CreateQuestionSerializer(serializers.Serializer):
-    course_id = serializers.IntegerField()
-    # created_by = serializers.IntegerField()
-    title = serializers.CharField(max_length=128)
-    content = serializers.CharField(max_length=1024 * 1024 * 8)
-    last_update_time = serializers.DateTimeField()
-    create_time = serializers.DateTimeField()
+class CreateQuestionSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Question
+        fields = "__all__"
 
 class EditQuestionSerializer(serializers.Serializer):
     id = serializers.IntegerField()
-    title = serializers.CharField(max_length=128)
-    content = serializers.CharField(max_length=1024 * 1024 * 8)
 
-class AnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = ("title", "content")
+
+
+class AnswerSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
     name = serializers.CharField()
     admin_type = serializers.CharField()
     content = serializers.CharField()
     last_update_time = serializers.DateTimeField()
 
-class CreateAnswerSerializer(serializers.ModelSerializer):
+class CreateAnswerSerializer(serializers.Serializer):
     question_id = serializers.IntegerField()
     content = serializers.CharField()
+    closure = serializers.BooleanField()
 
-class EditAnswerSerializer(serializers.ModelSerializer):
+class EditAnswerSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     content = serializers.CharField()
