c029b9cb5aff808b88780f8e115ca18f0088674d
DailyLinux <baksa1849@gmail.com>
2022-01-12 22:15:36 +0900

    Connect AssignmentList api

--- frontend/src/pages/prof/views/assignment/AssignmentList.vue
+++ frontend/src/pages/prof/views/assignment/AssignmentList.vue
@@ -14,7 +14,7 @@
             :fields="assignmentField"
             head-variant="light"
           >
-            <template #cell()="row">
+            <template #cell(show_detail)="row">
               <b-button size="sm" @click="row.toggleDetails" class="mb-2">
                 <b-icon icon="caret-right-fill" aria-hidden="true"></b-icon>
               </b-button>
@@ -88,15 +88,10 @@
         <b-button variant="primary" class="mb-2" v-b-modal.createAssignment>
           +Create
         </b-button>
-        <create-assignment-modal></create-assignment-modal>
-        <div class="pagination">
-          <b-pagination
-            v-model="currentPage"
-            :total-rows="assignmentList.length"
-            :per-page="perPage"
-            limit="3"
-        ></b-pagination>
-        </div>
+        <create-assignment-modal
+          :lecture-id="lectureId"
+          @update="updateAssignmentList"
+        ></create-assignment-modal>
         <div v-if="!assignmentList.length" v-text="center-align">
           No Assignment
         </div>
@@ -106,6 +101,7 @@
 </template>
 
 <script>
+import api from '../../api.js'
 import CreateAssignmentModal from './CreateAssignment.vue'
 import ImportPublicProblemModal from './ImportPublicProblem.vue'
 
@@ -115,17 +111,24 @@ export default {
     CreateAssignmentModal,
     ImportPublicProblemModal
   },
+  props: [
+    'lecture-id'
+  ],
   data () {
     return {
       assignmentField: [
         {
+          key: 'show_detail',
           label: ''
         },
         {
           key: 'title',
           label: 'Title'
         },
-        'Visible',
+        {
+          key: 'visible',
+          label: 'Visible'
+        },
         {
           key: 'status',
           label: 'Status'
@@ -133,52 +136,7 @@ export default {
         'Operation'
       ],
       assignmentList: [
-        {
-          id: 1,
-          title: '1주차 과제',
-          content: '<p>1주차 과제입니다.</p>',
-          created_by: {
-            id: 2,
-            username: 'youngHoon',
-            real_name: '김영훈'
-          },
-          course: {
-            id: 1,
-            title: 'Programming Basics',
-            course_code: 'GEDT018',
-            class_number: 41,
-            registered_year: '2021',
-            semester: 1
-          },
-          start_time: '2021-08-23T18:25:43+09:00',
-          end_time: '2021-08-25T19:25:43+09:00',
-          status: '-1',
-          submitted_problem: 8,
-          total_problem: 10
-        },
-        {
-          id: 2,
-          title: '2주차 과제',
-          content: '<p>2주차 과제입니다. </p>',
-          created_by: {
-            id: 2,
-            username: 'youngHoon',
-            real_name: '김영훈'
-          },
-          course: {
-            id: 1,
-            title: 'Computer Engineering',
-            course_code: 'GEDT018',
-            class_number: 41,
-            registered_year: '2021',
-            semester: 1
-          },
-          start_time: '2021-08-17T18:25:43+09:00',
-          end_time: '2021-08-23T19:25:43+09:00',
-          status: '0',
-          submitted_problem: 8,
-          total_problem: 10
-        }
+
       ],
       problemField: [
         {
@@ -210,8 +168,15 @@ export default {
     }
   },
   async mounted () {
+    this.lectureId = this.$route.params.lectureId
+    console.log(this.lectureId)
+    const res = await api.getAssignmentList(this.lectureId)
+    this.assignmentList = res.data.data.results
   },
   methods: {
+    async updateAssignmentList () {
+      api.getAssignmentList(this.lectureId)
+    }
   },
   computed: {
   }
@@ -219,12 +184,4 @@ export default {
 </script>
 
 <style lang="scss">
-  div{
-    &.pagination{
-    //margin-right: 5%;
-    //margin-top: 20px;
-    display: flex;
-    justify-content: flex-end;
-    }
-  }
 </style>
