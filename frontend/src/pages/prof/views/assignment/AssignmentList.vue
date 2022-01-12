b1c049c2ae23951b07bc874e9ebe3e8e5e5365f6
DailyLinux <baksa1849@gmail.com>
2022-01-12 22:15:36 +0900

    Initialize AssignmentList.vue.
    Revise assignmentList & make createAssi. modal
    
    .
    
    Add Import Problem Modal

--- frontend/src/pages/prof/views/assignment/AssignmentList.vue
+++ frontend/src/pages/prof/views/assignment/AssignmentList.vue
@@ -1,18 +1,212 @@
 <template>
-  <!-- Please split into many componenets !-->
-  <div>
-  </div>
+  <b-row
+    type="flex"
+    cols = "1"
+    id="dashboard"
+  >
+    <b-col>
+      <b-card title="Assignments">
+        <div class="table">
+          <b-table
+            borderless
+            hover
+            :items="assignmentList"
+            :fields="assignmentField"
+            head-variant="light"
+          >
+            <template #cell()="row">
+              <b-button size="sm" @click="row.toggleDetails" class="mb-2">
+                <b-icon icon="caret-right-fill" aria-hidden="true"></b-icon>
+              </b-button>
+            </template>
+            <template #row-details>
+              <b-table
+                borderless
+                hover
+                :items="problemList"
+                :fields="problemField"
+              >
+                <template #cell(operation)>
+                  <div>
+                    <icon-btn
+                      name="Edit"
+                      icon="pencil"
+                    />
+                    <icon-btn
+                      name="Save"
+                      icon="box-arrow-in-down"
+                    />
+                    <icon-btn
+                      name="Delete Contest"
+                      icon="trash"
+                    />
+                  </div>
+                </template>
+              </b-table>
+              <b-button size="sm" variant="warning" class="mr-2" v-b-modal.importProblem>
+                +problem<br>From SKKU coding platform
+              </b-button>
+              <import-public-problem-modal></import-public-problem-modal>
+              <b-button size="sm" variant="primary" class="mr-2">
+                +problem<br>Create a new problem
+              </b-button>
+              <div v-if="!problemList.length" v-text="center-align">
+                No Problem
+              </div>
+            </template>
+            <template #cell(title)="data">
+              {{ data.item.title }}
+            </template>
+            <template #cell(visible)>
+              <b-form-checkbox switch size="sm"></b-form-checkbox>
+            </template>
+            <template #cell(status)>
+              <b-icon
+                icon="circle-fill"
+                class="mb-2"
+              >
+              </b-icon>
+            </template>
+            <template #cell(operation)>
+              <div>
+                <icon-btn
+                  name="Edit"
+                  icon="pencil"
+                />
+                <icon-btn
+                  name="Save"
+                  icon="box-arrow-in-down"
+                />
+                <icon-btn
+                  name="Delete Contest"
+                  icon="trash"
+                />
+              </div>
+            </template>
+          </b-table>
+        </div>
+        <b-button variant="primary" class="mb-2" v-b-modal.createAssignment>
+          +Create
+        </b-button>
+        <create-assignment-modal></create-assignment-modal>
+        <div class="pagination">
+          <b-pagination
+            v-model="currentPage"
+            :total-rows="assignmentList.length"
+            :per-page="perPage"
+            limit="3"
+        ></b-pagination>
+        </div>
+        <div v-if="!assignmentList.length" v-text="center-align">
+          No Assignment
+        </div>
+      </b-card>
+    </b-col>
+  </b-row>
 </template>
 
 <script>
+import CreateAssignmentModal from './CreateAssignment.vue'
+import ImportPublicProblemModal from './ImportPublicProblem.vue'
 
 export default {
   name: 'AssignmentList',
   components: {
-    // Split into many components
+    CreateAssignmentModal,
+    ImportPublicProblemModal
   },
   data () {
     return {
+      assignmentField: [
+        {
+          label: ''
+        },
+        {
+          key: 'title',
+          label: 'Title'
+        },
+        'Visible',
+        {
+          key: 'status',
+          label: 'Status'
+        },
+        'Operation'
+      ],
+      assignmentList: [
+        {
+          id: 1,
+          title: '1주차 과제',
+          content: '<p>1주차 과제입니다.</p>',
+          created_by: {
+            id: 2,
+            username: 'youngHoon',
+            real_name: '김영훈'
+          },
+          course: {
+            id: 1,
+            title: 'Programming Basics',
+            course_code: 'GEDT018',
+            class_number: 41,
+            registered_year: '2021',
+            semester: 1
+          },
+          start_time: '2021-08-23T18:25:43+09:00',
+          end_time: '2021-08-25T19:25:43+09:00',
+          status: '-1',
+          submitted_problem: 8,
+          total_problem: 10
+        },
+        {
+          id: 2,
+          title: '2주차 과제',
+          content: '<p>2주차 과제입니다. </p>',
+          created_by: {
+            id: 2,
+            username: 'youngHoon',
+            real_name: '김영훈'
+          },
+          course: {
+            id: 1,
+            title: 'Computer Engineering',
+            course_code: 'GEDT018',
+            class_number: 41,
+            registered_year: '2021',
+            semester: 1
+          },
+          start_time: '2021-08-17T18:25:43+09:00',
+          end_time: '2021-08-23T19:25:43+09:00',
+          status: '0',
+          submitted_problem: 8,
+          total_problem: 10
+        }
+      ],
+      problemField: [
+        {
+          key: 'number',
+          label: '#'
+        },
+        {
+          key: 'type',
+          label: 'Type'
+        },
+        {
+          key: 'title',
+          label: 'Title'
+        },
+        {
+          key: 'submission',
+          lable: 'Submission'
+        },
+        'Operation'
+      ],
+      problemList: [
+        {
+          number: 'A',
+          type: 'Problem',
+          title: '가파른 경사',
+          submission: '58/60'
+        }
+      ]
     }
   },
   async mounted () {
@@ -25,5 +219,12 @@ export default {
 </script>
 
 <style lang="scss">
-  // Be careful of common css selector in admin/oj
+  div{
+    &.pagination{
+    //margin-right: 5%;
+    //margin-top: 20px;
+    display: flex;
+    justify-content: flex-end;
+    }
+  }
 </style>
---
+++ frontend/src/pages/prof/views/assignment/CreateAssignment.vue
@@ -0,0 +1,147 @@
+<template>
+  <b-modal
+  id="createAssignment"
+  title="Create New Assignment"
+  size="lg"
+  @ok="createNewAssignment"
+  @cancel="cancelCreation"
+  >
+    <b-form>
+      <b-form-group
+        id="input-group-assignment-title"
+        label="Assignment Title"
+        label-for="input-assignment-title"
+        required
+        :state="form.assignmentTitle !== ''"
+        invalid-feedback="Assignment Title is required"
+      >
+        <b-form-input
+          id="input-assignment-title"
+          v-model="form.assignmentTitle"
+          placeholder="Enter Title"
+          required
+          :state="form.assignmentTitle !== ''"
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group
+        id="input-group-assignment-content"
+        label="Assignment Content"
+        label-for="input-assignment-content"
+        required
+        :state="form.content !== ''"
+        invalid-feedback="Content is required"
+      >
+        <b-form-input
+          id="input-assignment-content"
+          v-model="form.assignmentContent"
+          placeholder="Enter Content"
+          required
+          :state="form.content !== ''"
+        ></b-form-input>
+      </b-form-group>
+      <b-row>
+        <b-col>
+          <b-form-group
+            id="input-group-start-time"
+            label="Start Time"
+            label-for="input-start-time"
+            required
+            :state="form.startTime !== ''"
+            invalid-feedback="Start Time is required"
+          >
+            <b-form-datepicker
+              id="input-start-time"
+              v-model="startDate"
+              today-button
+              close-button
+              type="datetime"
+              :date-format-options="{ year: 'numeric', month: '2-digit', day: '2-digit' }"
+              locale="en"
+              placeholder="Start Time"
+              style = "margin-bottom:10px"
+            />
+            <b-form-timepicker
+              v-model="startTime"
+              show-seconds
+              now-button
+            >
+            </b-form-timepicker>
+          </b-form-group>
+        </b-col>
+        <b-col>
+          <b-form-group
+            id="input-group-end-time"
+            label="End Time"
+            label-for="input-end-time"
+            required
+            :state="form.endTime !== ''"
+            invalid-feedback="End Time is required"
+          >
+            <b-form-datepicker
+              id="input-end-time"
+              v-model="endDate"
+              today-button
+              close-button
+              type="datetime"
+              :date-format-options="{ year: 'numeric', month: '2-digit', day: '2-digit' }"
+              locale="en"
+              placeholder="End Time"
+              style = "margin-bottom:10px"
+            />
+            <b-form-timepicker
+              v-model="endTime"
+              show-seconds
+              now-button
+            >
+            </b-form-timepicker>
+          </b-form-group>
+        </b-col>
+      </b-row>
+    </b-form>
+  </b-modal>
+</template>
+
+<script>
+// import api from '../../api.js'
+export default {
+  name: 'RegisterNewLectureModal',
+  components: {
+  },
+  data () {
+    return {
+      form: {
+        assignmentTitle: '',
+        content: '',
+        startTime: '',
+        endTime: ''
+      },
+      startDate: '',
+      startTime: '',
+      endDate: '',
+      endTime: ''
+    }
+  },
+  methods: {
+    async submitNewLecture () {
+      // await api.registerNewLecture(this.form)
+      alert(this.form)
+    },
+    initTime () {
+      ;[this.startDate, this.startTime] = this.form.start_time.split(/T|[+]/)
+      ;[this.endDate, this.endTime] = this.form.end_time.split(/T|[+]/)
+    },
+    setStartTime () {
+      this.form.start_time = this.startDate + ' ' + this.startTime
+    },
+    setEndTime () {
+      this.form.end_time = this.endDate + ' ' + this.endTime
+    }
+  },
+  computed: {
+  }
+}
+</script>
+
+<style lang="scss">
+  // Be careful of common css selector in admin/oj
+</style>
