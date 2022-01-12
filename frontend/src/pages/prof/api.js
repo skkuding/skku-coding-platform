b4ff540506226ac2426495cc7982d2531c1d8a7b
Jimin Ha <jimin9038@g.skku.edu>
2022-01-12 22:15:36 +0900

    Delete unused apis & erase getwebsiteconfig (prof)

--- frontend/src/pages/prof/api.js
+++ frontend/src/pages/prof/api.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import router from './router'
 import axios from 'axios'
-import utils from '@/utils/utils'
 
 Vue.prototype.$http = axios
 axios.defaults.baseURL = '/api'
@@ -9,7 +8,6 @@ axios.defaults.xsrfHeaderName = 'X-CSRFToken'
 axios.defaults.xsrfCookieName = 'csrftoken'
 
 export default {
-  // 登录
   login (username, password) {
     return ajax('login', 'post', {
       data: {
@@ -24,158 +22,8 @@ export default {
   getProfile () {
     return ajax('profile', 'get')
   },
-  // 获取公告列表
-  getAnnouncementList (offset, limit) {
-    return ajax('admin/announcement', 'get', {
-      params: {
-        paging: true,
-        offset,
-        limit
-      }
-    })
-  },
-  // 删除公告
-  deleteAnnouncement (id) {
-    return ajax('admin/announcement', 'delete', {
-      params: {
-        id
-      }
-    })
-  },
-  // 修改公告
-  updateAnnouncement (data) {
-    return ajax('admin/announcement', 'put', {
-      data
-    })
-  },
-  // 添加公告
-  createAnnouncement (data) {
-    return ajax('admin/announcement', 'post', {
-      data
-    })
-  },
-  // 获取用户列表
-  getUserList (offset, limit, keyword) {
-    const params = { paging: true, offset, limit }
-    if (keyword) {
-      params.keyword = keyword
-    }
-    return ajax('admin/user', 'get', {
-      params: params
-    })
-  },
-  // 获取单个用户信息
-  getUser (id) {
-    return ajax('admin/user', 'get', {
-      params: {
-        id
-      }
-    })
-  },
-  // 编辑用户
-  editUser (data) {
-    return ajax('admin/user', 'put', {
-      data
-    })
-  },
-  deleteUsers (id) {
-    return ajax('admin/user', 'delete', {
-      params: {
-        id
-      }
-    })
-  },
-  importUsers (users) {
-    return ajax('admin/user', 'post', {
-      data: {
-        users
-      }
-    })
-  },
-  generateUser (data) {
-    return ajax('admin/generate_user', 'post', {
-      data
-    })
-  },
   getLanguages () {
     return ajax('languages', 'get')
-  },
-  getSMTPConfig () {
-    return ajax('admin/smtp', 'get')
-  },
-  createSMTPConfig (data) {
-    return ajax('admin/smtp', 'post', {
-      data
-    })
-  },
-  editSMTPConfig (data) {
-    return ajax('admin/smtp', 'put', {
-      data
-    })
-  },
-  testSMTPConfig (email) {
-    return ajax('admin/smtp_test', 'post', {
-      data: {
-        email
-      }
-    })
-  },
-  getWebsiteConfig () {
-    return ajax('admin/website', 'get')
-  },
-  editWebsiteConfig (data) {
-    return ajax('admin/website', 'post', {
-      data
-    })
-  },
-  getJudgeServer () {
-    return ajax('admin/judge_server', 'get')
-  },
-  deleteJudgeServer (hostname) {
-    return ajax('admin/judge_server', 'delete', {
-      params: {
-        hostname: hostname
-      }
-    })
-  },
-  updateJudgeServer (data) {
-    return ajax('admin/judge_server', 'put', {
-      data
-    })
-  },
-  getInvalidTestCaseList () {
-    return ajax('admin/prune_test_case', 'get')
-  },
-  pruneTestCase (id) {
-    return ajax('admin/prune_test_case', 'delete', {
-      params: {
-        id
-      }
-    })
-  },
-  createContest (data) {
-    return ajax('admin/contest', 'post', {
-      data
-    })
-  },
-  getContest (id) {
-    return ajax('admin/contest', 'get', {
-      params: {
-        id
-      }
-    })
-  },
-  editContest (data) {
-    return ajax('admin/contest', 'put', {
-      data
-    })
-  },
-  deleteContest (id) {
-    return ajax('admin/contest', 'delete', {
-      params: {
-        id
-      }
-    })
   },
   getAssignmentSubmission (assignmentId, problemId, offset, limit) {
     return ajax('/assignment_submissions_professor', 'get', {
@@ -187,30 +35,6 @@ export default {
       }
     })
   },
-  getContestAnnouncementList (contestID) {
-    return ajax('admin/contest/announcement', 'get', {
-      params: {
-        contest_id: contestID
-      }
-    })
-  },
-  createContestAnnouncement (data) {
-    return ajax('admin/contest/announcement', 'post', {
-      data
-    })
-  },
-  deleteContestAnnouncement (id) {
-    return ajax('admin/contest/announcement', 'delete', {
-      params: {
-        id
-      }
-    })
-  },
-  updateContestAnnouncement (data) {
-    return ajax('admin/contest/announcement', 'put', {
-      data
-    })
-  },
   getProblemTagList () {
     return ajax('problem/tags', 'get')
   },
@@ -219,77 +43,6 @@ export default {
       data
     })
   },
-  createProblem (data) {
-    return ajax('admin/problem', 'post', {
-      data
-    })
-  },
-  editProblem (data) {
-    return ajax('admin/problem', 'put', {
-      data
-    })
-  },
-  deleteProblem (id) {
-    return ajax('admin/problem', 'delete', {
-      params: {
-        id
-      }
-    })
-  },
-  getProblem (id) {
-    return ajax('admin/problem', 'get', {
-      params: {
-        id
-      }
-    })
-  },
-  getProblemList (params) {
-    params = utils.filterEmptyValue(params)
-    return ajax('admin/problem', 'get', {
-      params
-    })
-  },
-  getContestProblemList (params) {
-    params = utils.filterEmptyValue(params)
-    return ajax('admin/contest/problem', 'get', {
-      params
-    })
-  },
-  getContestProblem (id) {
-    return ajax('admin/contest/problem', 'get', {
-      params: {
-        id
-      }
-    })
-  },
-  createContestProblem (data) {
-    return ajax('admin/contest/problem', 'post', {
-      data
-    })
-  },
-  editContestProblem (data) {
-    return ajax('admin/contest/problem', 'put', {
-      data
-    })
-  },
-  deleteContestProblem (id) {
-    return ajax('admin/contest/problem', 'delete', {
-      params: {
-        id
-      }
-    })
-  },
-  makeContestProblemPublic (data) {
-    return ajax('admin/contest_problem/make_public', 'post', {
-      data
-    })
-  },
-  getReleaseNotes () {
-    return ajax('admin/versions', 'get')
-  },
-  // getDashboardInfo () {
-  //   return ajax('admin/dashboard_info', 'get')
-  // },
   getSessions () {
     return ajax('sessions', 'get')
   },
@@ -298,9 +51,6 @@ export default {
       data
     })
   },
-  // getCourseList () {
-  //   return ajax('lecture/course', 'get')
-  // },
   getTestCase (id) {
     return ajax('admin/testcase_text', 'get', {
       params: {
@@ -334,20 +84,8 @@ export default {
       data
     })
   },
-  getAssignmentList (courseId, assignmentId, limit, offset) {
-    const params = { course_id: courseId, assignment_id: assignmentId, limit, offset }
-    return ajax('/lecture/professor/course/assignment', 'get', {
-      params: params
-    })
-  },
   getCourseStudents (courseId, limit, offset) {
     const params = { course_id: courseId, limit, offset }
-    return ajax('/lecture/professor/course/students', 'get', {
-      params: params
-    })
-  },
-  getCourseStudentTotal (courseId, count) {
-    const params = { course_id: courseId, count }
     return ajax('/lecture/professor/course/students', 'get', {
       params: params
     })
@@ -363,14 +101,10 @@ export default {
       params: params
     })
   },
-  editStudent (data) {
-    return ajax('/lecture/professor/course/students', 'delete', {
-      data
-    })
-  },
-  postAssignment (data) { // 테스트용 api
-    return ajax('/lecture/professor/course/assignment', 'post', {
-      data
+  getAssignmentList (courseId, assignmentId, limit, offset) {
+    const params = { course_id: courseId, assignment_id: assignmentId, limit, offset }
+    return ajax('/lecture/professor/course/assignment', 'get', {
+      params: params
     })
   },
   createAssignment (data) {
@@ -426,17 +160,6 @@ export default {
   editSubmissionScore (data) {
     return ajax('/edit_submission_score', 'put', {
       data
-    })
-  },
-  downloadSubmissions (assignmentId, problemId, excludeAdmin, lastSubmission) {
-    const params = {
-      assignment_id: assignmentId,
-      problem_id: problemId,
-      exclude_admin: excludeAdmin,
-      last_submission: lastSubmission
-    }
-    return ajax('/lecture/professor/download_submissions', 'get', {
-      params
     })
   }
 }
