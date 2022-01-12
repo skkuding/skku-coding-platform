0853bc0f8f044e3b0fee556ba2d812f66f15a9a3
Jimin Ha <jimin9038@g.skku.edu>
2022-01-12 22:15:37 +0900

    Delete unused variables

--- frontend/src/pages/prof/views/problem/ProblemGrade.vue
+++ frontend/src/pages/prof/views/problem/ProblemGrade.vue
@@ -155,16 +155,9 @@ export default {
   methods: {
     async downloadAllSubmissions () {
       const url = 'lecture/professor/download_submissions?problem_id=' + this.problemId + '&assignment_id=' + this.assignmentId + '&exclude_admin=' + Number(this.downloadExcludeAdmin) + '&last_submission=' + Number(this.downloadOnlyLastSubmissions)
-      utils.downloadFile(url)
+      await utils.downloadFile(url)
       this.downloadOnlyLastSubmissions = false
       this.downloadExcludeAdmin = false
-    },
-    goCode (id) {
-      this.$router.push({ name: 'announcement', params: { id } })
-    },
-    currentChange (page) {
-      this.currentPage = page
-      this.getSubmissionList(page)
     },
     async editSubmissionScore (submissionId, newScore, row) {
       const data = {
