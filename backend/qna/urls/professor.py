7d8356f5d6b80d900aef2620ec0b35b52e0af314
aintbe <drwho@g.skku.edu>
2022-01-12 17:13:09 +0900

    Resolve linter flags

--- backend/qna/urls/professor.py
+++ backend/qna/urls/professor.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from ..views.professor import *
+from ..views.professor import QuestionAPI
 
 urlpatterns = [
     path("course/question", QuestionAPI.as_view(), name="question_professor_api"),
