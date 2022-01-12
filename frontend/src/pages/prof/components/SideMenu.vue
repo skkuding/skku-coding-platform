2466cab3906b0a91a619aa4f616edf2dc6c6c308
Jimin Ha <jimin9038@g.skku.edu>
2022-01-12 22:15:36 +0900

    Change Sidemenu logic & design

--- frontend/src/pages/prof/components/SideMenu.vue
+++ frontend/src/pages/prof/components/SideMenu.vue
@@ -1,177 +1,153 @@
 <template>
-  <b-list-group class="prof_vertical_menu" v-show="sideMenuShow">
-    <b-list-group-item class="logo">
-      <img
-        src="@/assets/logos/logo.png"
-        style="height=35px; width=auto;"
-        alt="oj admin"
-      >
-    </b-list-group-item>
-    <b-list-group-item class="put-in">
-      My Lecture
-      <b-button class="put-in-button" variant="white" @click="$emit('hide')">
-        <b-icon icon="chevron-double-left" style="margin-left: 45px"/>
-      </b-button>
-    </b-list-group-item>
-    <b-list-group-item to="/">
-      <b-icon icon="house"/>
-      Home
-    </b-list-group-item>
-
-    <div v-for="(term,index) in lecture_term" :key="index">
-      <b-list-group-item
-        class="list-group-subitem"
-        v-b-toggle="term.year + term.semester"
-      >
+  <div id="sidebar-wrapper">
+    <b-list-group class="prof_vertical_menu" v-show="sideMenuShow">
+      <!-- <b-list-group-item class="put-in" style="font-size: 20px;padding-top: 2rem;">
+        SKKU Coding Platform
+      </b-list-group-item> -->
+      <img id="coding-platform-logo" src="@/assets/logos/codingPlatformLogo.png" alt="coding-platform-logo">
+      <b-list-group-item>
+        <div style="width: 100%;height:1px;background-color: #B8B8B8;"></div>
+      </b-list-group-item>
+      <b-list-group-item to="/" class="list-group-subitem">
         <b-icon
-          :icon="click ? 'caret-right-fill' : 'caret-down-fill'"
-          font-scale="1"
+          icon="house"
           style="margin-right: 8px"
         />
-        {{term.year}} {{semester_name[term.semester]}}
+        Home
       </b-list-group-item>
 
-      <div v-for="(lecture,index) in lectures" :key="index">
-        <div v-if="term.year === lecture.registered_year && term.semester === lecture.semester">
+      <div v-for="(term,index) in registeredTerms" :key="index">
+        <b-list-group-item
+          class="list-group-subitem"
+        >
+          <b-icon
+            :icon="click ? 'caret-right-fill' : 'caret-down-fill'"
+            font-scale="1"
+            style="margin-right: 8px; cursor:pointer"
+            v-b-toggle="String(term.registered_year) + '-' + String(term.semester)"
+          />
+          <span v-b-toggle="String(term.registered_year) + '-' + String(term.semester)"> {{term.registered_year}} {{semester_name[term.semester]}} </span>
+        </b-list-group-item>
 
-          <b-collapse :id="term.year + term.semester" role="tab">
-            <b-list-group-item
-              class="list-group-lecture"
-              v-b-toggle="'inner'+lecture.id"
-            >
-              <b-icon
-                :icon="click ? 'caret-right-fill' : 'caret-down-fill'"
-                font-scale="1"
-                style="margin-right: 8px"
-              />
-              {{lecture.title}}_{{lecture.course_code}}_{{lecture.class_number}}
-            </b-list-group-item>
+        <div v-for="(lecture,index) in lectures" :key="index">
+          <div v-if="term.registered_year === lecture.registered_year && term.semester === lecture.semester">
 
-            <b-collapse :id="'inner'+lecture.id" role="tabpanel">
+            <b-collapse :id="String(term.registered_year) + '-' + String(term.semester)">
               <b-list-group-item
-                :to="'/lecture/'+lecture.id+'/dashboard'"
-                class="list-group-inner"
+                class="list-group-lecture"
               >
                 <b-icon
-                  icon="record-fill"
-                  font-scale="0.5"
-                  style="margin-right: 8px"
+                  :icon="click ? 'caret-right-fill' : 'caret-down-fill'"
+                  font-scale="1"
+                  style="margin-right: 8px; cursor:pointer"
+                  v-b-toggle="'inner'+lecture.id"
                 />
-                Dashboard
+                <span v-b-toggle="'inner'+lecture.id">{{lecture.title}}_{{lecture.course_code}}_{{lecture.class_number}}</span>
               </b-list-group-item>
-              <b-list-group-item
-                :to="'/lecture/'+lecture.id+'/assignments'"
-                class="list-group-inner"
-              >
-                <b-icon
-                  icon="record-fill"
-                  font-scale="0.5"
-                  style="margin-right: 8px"
-                />
-                Assignments
-              </b-list-group-item>
-              <b-list-group-item
-                :to="'/lecture/'+lecture.id+'/qna'"
-                class="list-group-inner"
-              >
-                <b-icon
-                  icon="record-fill"
-                  font-scale="0.5"
-                  style="margin-right: 8px"
-                />
-                Questions
-              </b-list-group-item>
+
+              <b-collapse :id="'inner'+lecture.id" role="tabpanel">
+                <b-list-group-item
+                  :to="'/lecture/'+lecture.id+'/dashboard'"
+                  class="list-group-inner"
+                >
+                  <b-icon
+                    icon="record-fill"
+                    font-scale="0.5"
+                    style="margin-right: 8px;"
+                  />
+                  Dashboard
+                </b-list-group-item>
+                <b-list-group-item
+                  :to="'/lecture/'+lecture.id+'/assignment'"
+                  class="list-group-inner"
+                >
+                  <b-icon
+                    icon="record-fill"
+                    font-scale="0.5"
+                    style="margin-right: 8px "
+                  />
+                  Assignments
+                </b-list-group-item>
+                <b-list-group-item
+                  :to="'/lecture/'+lecture.id+'/qna'"
+                  class="list-group-inner"
+                >
+                  <b-icon
+                    icon="record-fill"
+                    font-scale="0.5"
+                    style="margin-right: 8px"
+                  />
+                  Questions
+                </b-list-group-item>
+              </b-collapse>
             </b-collapse>
-          </b-collapse>
+          </div>
         </div>
       </div>
-    </div>
-  </b-list-group>
+    </b-list-group>
+  </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
 import api from '../api.js'
 export default {
   name: 'SideMenu',
   data () {
     return {
       currentPath: '',
-      lecture_term: [],
+      registeredTerms: [],
       click: true,
       semester_name: ['Spring', 'Summer', 'Fall', 'Winter'],
       lectures: [],
-      lectureNumber: 1
+      lectureNumber: 1,
+      sideMenuShow: true
     }
   },
   mounted () {
     this.currentPath = this.$route.path
-    this.lectureGroup()
+    this.groupLectures()
   },
   computed: {
-    ...mapGetters(['user', 'isSuperAdmin', 'hasProblemPermission'])
   },
   methods: {
     putMenuInside () {
       this.sideMenuShow = false
     },
-    lectureGroup () {
-      const res = api.getCourseList() // id, limit, offset=0 
-      const apilectures = res.data.data.results
-      /* const apilectures = [{ // 여기서 api 불러옴
-        id: 1,
-        title: 'Python Programming',
-        course_code: 'GDBEASDF',
-        class_number: 41,
-        created_by: {
-          id: 1,
-          username: 'minchae',
-          real_name: '고민채'
-        },
-        registered_year: '2021',
-        semester: 0
-      },
-      {
-        id: 2,
-        title: '프로그래밍 기초와 실습',
-        course_code: 'GDBEAPOI',
-        class_number: 40,
-        created_by: {
-          id: 1,
-          username: 'minchae',
-          real_name: '고민채'
-        },
-        registered_year: '2020',
-        semester: 1
-      }]
-      */
-      const registerTerm = []
-      apilectures.sort((a, b) => { return a.registered_year < b.registered_year })
-      apilectures.sort((a, b) => {
-        if (a.registered_year === b.registered_year) {
-          return a.semester < b.semester
-        } else { return 0 }
+    async groupLectures () {
+      const res = await api.getCourseList() // id, limit, offset=0
+      const lectures = res.data.data.results
+      // Make registered term unique list
+      const registeredTerms = new Set()
+      lectures.forEach(lecture => {
+        registeredTerms.add(JSON.stringify({
+          semester: lecture.semester,
+          registered_year: lecture.registered_year
+        }))
       })
-      apilectures.sort((a, b) => { return a.title < b.title })
-      apilectures.sort((a, b) => {
-        if (a.title === b.title) {
-          return a.class_number < b.class_number
-        } else { return 0 }
-      })
-      apilectures.forEach(lecture => {
-        if (!({ year: lecture.registered_year, semester: lecture.semester } in registerTerm)) {
-          registerTerm.push({ year: lecture.registered_year, semester: lecture.semester })
-        }
-      })
-
-      this.lectures = apilectures
-      this.lecture_term = registerTerm
+      this.registeredTerms = Array.from(registeredTerms).map(term =>
+        JSON.parse(term)
+      )
+      console.log(this.registeredTerms)
+      this.lectures = lectures
     }
   }
 }
 </script>
 
 <style scoped lang="scss">
+  #sidebar-wrapper {
+    min-height:100vh;
+    width:15%;
+    min-width:270px;
+    height:100%;
+    background-color: white;
+    word-break: break-all;
+    overflow-y: scroll;
+  }
+  #coding-platform-logo {
+    display: block;
+    margin:auto;
+  }
   #prof_vertical_menu {
     flex-grow: 1;
     flex-shrink: 1;
@@ -181,10 +157,6 @@ export default {
     height: 100%;
     z-index: 1;
     background-color: white;
-    .logo {
-      margin: 20px 0;
-      text-align: center;
-    }
     .put-in {
       display: flex;
       flex-direction: row;
@@ -197,12 +169,12 @@ export default {
     }
   }
   .list-group-subitem {
-    padding: 16px 16px 16px 50px;
+    padding: 10px 16px 10px 30px;
   }
   .list-group-lecture {
-    padding: 16px 16px 16px 70px;
+    padding: 5px 16px 5px 50px;
   }
   .list-group-inner {
-    padding: 16px 16px 16px 90px;
+    padding: 5px 16px 5px 70px;
   }
 </style>
