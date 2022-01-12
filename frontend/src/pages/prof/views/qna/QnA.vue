4baf4b24d82a4482a9b0952d23c2baa99da76f68
Kohminchae <mc0jooo@gmail.com>
2022-01-12 22:15:36 +0900

    sidemenu QnA

--- frontend/src/pages/prof/views/qna/QnA.vue
+++ frontend/src/pages/prof/views/qna/QnA.vue
@@ -1,10 +1,40 @@
 <template>
-  <!-- Please split into many componenets !-->
-  <div>
+  <div class="contest-list-card font-bold">
+    <div class="top-bar mb-4">
+      <h2 class="title">All Contest</h2>
+    </div>
+    <b-card >
+      <div class="table">
+        <b-table
+          hover
+          :items="qna"
+          :fields="qnalist"
+          :per-page="perPage"
+          :current-page="currentPage"
+          head-variant="light"
+          @row-clicked="goContest"
+        >
+        </b-table>
+      </div>
+      <div
+        v-if="!qnalist.length"
+      >
+        No Question
+      </div>
+    </b-card>
+    <div class="pagination">
+      <b-pagination
+        v-model="currentPage"
+        :total-rows="contest.length"
+        :per-page="perPage"
+        limit="3"
+      ></b-pagination>
+    </div>
   </div>
 </template>
 
 <script>
+// import api from '../../api.js'
 
 export default {
   name: 'QnA',
@@ -13,6 +43,9 @@ export default {
   },
   data () {
     return {
+      qnafield: ['Title', 'Status', 'Date'],
+      qnalist: [
+      ]
     }
   },
   async mounted () {
