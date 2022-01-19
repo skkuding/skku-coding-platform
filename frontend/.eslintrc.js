04e65b13517e96d81be8cecb867c555bef810b8a
Jimin Ha <jimin9038@g.skku.edu>
2022-01-19 12:04:08 +0900

    feat: setting eslint cypress plugin

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -5,7 +5,8 @@ module.exports = {
   },
   extends: [
     'plugin:vue/essential',
-    '@vue/standard'
+    '@vue/standard',
+    'plugin:cypress/recommended'
   ],
   parserOptions: {
     parser: '@babel/eslint-parser'
