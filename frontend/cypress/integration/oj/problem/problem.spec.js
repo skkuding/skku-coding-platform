bece29243e9306c72a4e20b1d5922a062e7be8c7
Jimin Ha <jimin9038@g.skku.edu>
2022-01-23 08:33:43 +0000

    Translate korean fixture to english, minor fix
    - cypress run command 를 쓰기 위해서 package.json의 cy script를 바꿈
    -

--- frontend/cypress/integration/oj/problem/problem.spec.js
+++ frontend/cypress/integration/oj/problem/problem.spec.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 
-context('Problem', () => {
+// describe -> context -> it
+context('When user tries to submit a problem (not in contest)', () => {
   beforeEach(() => {
     cy.loginSuperAdmin()
     cy.visit('problem')
@@ -14,27 +15,4 @@ context('Problem', () => {
       cy.get('[data-cy="button-submit"]').click()
     })
   })
-  // it('Make New problem - Minimum fields included', () => {
-  //   cy.get('.list-group-item').contains('Problem').click()
-  //   cy.get('.list-group-item').contains('Problem List').click()
-  //   cy.contains('+ Create').click()
-  //   const randomString = Math.random().toString(36)
-  //   cy.get('#input-display-id').type(randomString.substring(3, 10))
-  //   cy.get('#input-title').type('Make problem Test' + randomString.substring(3, 20))
-  //   cy.get('[data-cy="input-description"] > .ProseMirror').type('Problem Description' + randomString.substring(3, 100))
-  //   cy.get('[data-cy="input-input-description"] > .ProseMirror').type('Problem Input Description' + randomString.substring(3, 100))
-  //   cy.get('[data-cy="input-output-description"] > .ProseMirror').type('Problem Output Description' + randomString.substring(3, 100))
-
-  //   cy.get('.tag-dropdown').contains('tag').click()
-  //   cy.get('[data-cy="input-add-tag"]').type(randomString.substring(3, 6))
-  //   cy.get('[data-cy="button-add-tag"]').click()
-
-  //   cy.get('[data-cy="input-input-samples1"]').type('Problem Input Description' + randomString.substring(3, 100))
-  //   cy.get('[data-cy="input-output-samples1"]').type('Problem Output Description' + randomString.substring(3, 100))
-
-  //   cy.get('[data-cy="input-input-testcase1"]').type('Problem Input Description' + randomString.substring(3, 100))
-  //   cy.get('[data-cy="input-output-testcase1"]').type('Problem Output Description' + randomString.substring(3, 100))
-
-  //   cy.get('[data-cy="button-save"]').click()
-  // })
 })
