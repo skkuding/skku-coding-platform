79170ffbf3ad24e654a9335cb65886a11464eb60
goo314 <rn0731@naver.com>
2022-01-29 01:58:21 +0900

    fix: Add component communication(props, event)

--- frontend/src/pages/oj/components/Pagination.vue
+++ frontend/src/pages/oj/components/Pagination.vue
@@ -1,51 +1,57 @@
 <template>
-  <div class="pagination">
-    <div class="page-itm">
-      <div @click="changePage(1)" class="page-btn">&lt;&lt;</div>
-      <div @click="changePage(currentPage-1)" class="page-btn">&lt;</div>
-      <div
-        v-for="page in pageList"
-        :key="page"
-        @click="changePage(page)"
-        :class="[ page==currentPage? 'page-btn select': 'page-btn' ]">
-        {{page}}
-        </div>
-      <div @click="changePage(currentPage+1)" class="page-btn">&gt;</div>
-      <div @click="changePage(numberOfPages)" class="page-btn">&gt;&gt;</div>
-    </div>
+  <div class="page-itm">
+    <div @click="changePage(1)" class="page-btn leftedge">«</div>
+    <div @click="changePage(currentPage-1)" class="page-btn">&lt;</div>
+    <div
+      v-for="page in pageList"
+      :key="page"
+      @click="changePage(page)"
+      :class="[ page==currentPage? 'page-btn select': 'page-btn' ]">
+      {{page}}
+      </div>
+    <div @click="changePage(currentPage+1)" class="page-btn">&gt;</div>
+    <div @click="changePage(numberOfPages)" class="page-btn rightedge">»</div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Pagination',
-  props: {},
+  props: {
+    totalRows: { // number of total rows in table
+      type: Number
+    },
+    perPage: { // number of rows in table per one page
+      type: Number
+    },
+    limit: {
+      type: String
+    }
+  },
   data () {
     return {
-      rows: 45, // number of total rows in table
-      perPage: 3, // number of rows in table per one page
-      currentPage: 1,
-      limit: 3 // maximum of number of pages
+      currentPage: 1
     }
   },
   methods: {
     changePage (page) {
       if (page >= 1 && page <= this.numberOfPages) {
         this.currentPage = page
+        this.$emit('input', this.currentPage)
       }
     }
   },
   computed: {
-    numberOfPages () { // number of pages (if 9 rows and 3 for each pages, then 3 pages)
-      return Math.ceil(this.rows / this.perPage)
+    numberOfPages () { // number of pages
+      return Math.ceil(this.totalRows / this.perPage)
     },
     startPage () {
       var start = (Math.trunc((this.currentPage - 1) / this.limit)) * this.limit + 1
       return start
     },
     endPage () {
-      var end = this.startPage + this.limit - 1
-      return end < this.numberOfPages ? end : this.numberOfPages
+      var end = this.startPage + Number(this.limit) - 1
+      return end <= this.numberOfPages ? end : this.numberOfPages
     },
     pageList () {
       var pages = []
@@ -59,41 +65,44 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.pagination {
-  width: 95%;
-  margin-right: 10%;
-  margin-top: 10px;
-  display: flex;
-  justify-content: flex-end !important;
-}
-
 .page-itm {
-  border-radius: 50rem !important;
+  width: 95%;
   margin: 20px 5% 16px 0;
   display: flex;
+  justify-content: flex-end !important;
   flex-direction: row;
 }
 
 .page-btn {
-  width: 30px;
-  height: 30px;
+  width: 35px;
+  height: 38px;
   text-align: center;
-  line-height: 25px;
-  margin-left: 0.25rem;
-  border-radius: 5px !important;
+  margin-left: -1px;
+  line-height: 35px;
   color: #bdbdbd;
-  border: thin solid #bdbdbd;
+  border: thin solid #dadada;
   cursor: pointer;
+}
+
+.page-btn.leftedge {
+  border-top-left-radius: 0.25rem !important;
+  border-bottom-left-radius: 0.25rem !important;
+}
+
+.page-btn.rightedge {
+  border-top-right-radius: 0.25rem !important;
+  border-bottom-right-radius: 0.25rem !important;
 }
 
 .page-btn.select {
     background-color: #bdbdbd;
+    border: thin solid #bdbdbd;
     color: white;
+    pointer-events: none;
 }
 
 .page-btn:hover {
-  background-color: #bdbdbd;
-  color: white;
+  background-color: #e9ecee;
 }
 
 </style>
