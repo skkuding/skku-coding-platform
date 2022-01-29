627fbb9fb5128fd22257549249030cddf45ab9f3
Jaemin Choi <1dotolee@gmail.com>
2022-01-29 08:12:50 +0000

    feat: new homepage design
    banner 사진을 없애고 gradient를 적용한 heading을 사용한다.

--- frontend-new/src/pages/index.vue
+++ frontend-new/src/pages/index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { Head } from '@vueuse/head'
-import src from '~/assets/banner.png?h=613&webp'
+import src from '~/assets/home.png?w=280&webp'
 </script>
 
 <template>
@@ -10,9 +10,30 @@ import src from '~/assets/banner.png?h=613&webp'
       name="description"
       content="SKKU Coding Platform helps you enhance your coding ability, providing competitive programming environment."
     >
+    <link
+      href="https://fonts.googleapis.com/css2?family=Inter:wght@800&text=SKKUCodingPlatform&display=swap"
+      rel="stylesheet"
+    >
   </Head>
-  <img
-    :src="src"
-    class="w-full"
-  >
+  <main class="flex flex-col items-center m-8">
+    <h1
+      class="
+        py-8 font-extrabold text-6xl font-[Inter]
+        bg-clip-text text-transparent bg-gradient-to-r
+        from-sky-500 to-lime-500"
+    >
+      SKKU Coding Platform
+    </h1>
+    <p
+      class="
+        mb-8 font-medium text-stone-500 text-xl
+      "
+    >
+      Your Coding Skills Evolves Here.
+    </p>
+    <img
+      class="my-8"
+      :src="src"
+    >
+  </main>
 </template>
