606b1c53d4d30ed9b0aea94cad3ddce475f62500
Jimin Ha <jimin9038@g.skku.edu>
2022-01-29 16:23:42 +0000

    fix: unify api, model names & field

--- backend/group/urls/oj.py
+++ backend/group/urls/oj.py
@@ -3,7 +3,7 @@ from ..views.oj import GroupRegistrationRequestAPI, GroupAPI, GroupDetailAPI
 
 
 urlpatterns = [
-    path("group/register_request", GroupRegistrationRequestAPI.as_view(), name="group_register_request_api"),
+    path("group/registration_request", GroupRegistrationRequestAPI.as_view(), name="group_registration_request_api"),
     path("group", GroupAPI.as_view(), name="group_api"),
     path("group/detail", GroupDetailAPI.as_view(), name="group_detail_api")
 ]
