f5152abdd52053c3533300756390b57625acb181
Jimin Ha <jimin9038@g.skku.edu>
2022-02-05 01:54:43 +0000

    feat: get admin members in group api detail resp

--- backend/group/views/oj.py
+++ backend/group/views/oj.py
@@ -74,6 +74,9 @@ class GroupAPI(APIView):
             return self.error("Group does not exist")
         data = GroupDetailSerializer(group).data
 
+        data["members"] = GroupMemberSerializer(GroupMember.objects.filter(group=group_id, is_group_admin=False), many=True).data
+        data["admin_members"] = GroupMemberSerializer(GroupMember.objects.filter(group=group_id, is_group_admin=True), many=True).data
+
         if GroupMember.objects.filter(is_group_admin=True, group=group, user=user).exists():
             group_member_join = GroupMemberJoin.objects.filter(group=group)
             data["group_member_join"] = GroupMemberJoinSerializer(group_member_join, many=True).data
