0d9913199388721b0ef0f67af14f51646737b23f
goo314 <rn0731@naver.com>
2022-02-08 12:23:05 +0000

    feat: Create USerContestAPI

--- backend/contest/urls/oj.py
+++ backend/contest/urls/oj.py
@@ -3,6 +3,7 @@ from django.urls import path
 from ..views.oj import ContestAnnouncementListAPI
 from ..views.oj import ContestPasswordVerifyAPI, ContestAccessAPI
 from ..views.oj import ContestListAPI, ContestAPI, ContestRankAPI
+from ..views.oj import UserContestAPI
 
 urlpatterns = [
     path("contests/", ContestListAPI.as_view(), name="contest_list_api"),
@@ -11,4 +12,5 @@ urlpatterns = [
     path("contest/announcement/", ContestAnnouncementListAPI.as_view(), name="contest_announcement_api"),
     path("contest/access/", ContestAccessAPI.as_view(), name="contest_access_api"),
     path("contest/rank/", ContestRankAPI.as_view(), name="contest_rank_api"),
+    path("contest/user/", UserContestAPI.as_view(), name="contest_user_api"),
 ]
