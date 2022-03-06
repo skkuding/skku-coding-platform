6b96b2c6b2052ecb5a371aa500f7777a4426edba
minseo999 <alstj778@gmail.com>
2022-03-06 13:46:00 +0000

    Add problemBank post API

--- backend/contest/urls/oj.py
+++ backend/contest/urls/oj.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from ..views.oj import ContestAnnouncementListAPI
 from ..views.oj import ContestPasswordVerifyAPI, ContestAccessAPI
-from ..views.oj import ContestListAPI, ContestAPI, ContestRankAPI
+from ..views.oj import ContestListAPI, ContestAPI, ContestRankAPI, ProblemBankAPI
 
 urlpatterns = [
     path("contests/", ContestListAPI.as_view(), name="contest_list_api"),
@@ -11,4 +11,5 @@ urlpatterns = [
     path("contest/announcement/", ContestAnnouncementListAPI.as_view(), name="contest_announcement_api"),
     path("contest/access/", ContestAccessAPI.as_view(), name="contest_access_api"),
     path("contest/rank/", ContestRankAPI.as_view(), name="contest_rank_api"),
+    path("contest/bank/", ProblemBankAPI.as_view(), name="contest_bank_api"),
 ]
