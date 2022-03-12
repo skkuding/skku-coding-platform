594f21c1b2f6739eb206c203b095b6dcddd56b5d
minseo999 <alstj778@gmail.com>
2022-03-12 09:05:21 +0000

    fix: add bank field to CreateOrEditProblemSerializer

--- backend/problem/serializers.py
+++ backend/problem/serializers.py
@@ -70,6 +70,7 @@ class CreateOrEditProblemSerializer(serializers.Serializer):
     hint = serializers.CharField(allow_blank=True, allow_null=True)
     source = serializers.CharField(max_length=256, allow_blank=True, allow_null=True)
     share_submission = serializers.BooleanField()
+    bank = serializers.BooleanField()
 
 
 class TestCaseTextSerializer(serializers.Serializer):
