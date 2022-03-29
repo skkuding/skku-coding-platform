c1c34bc7a4ed98b674be74fe2e9b2abe8cefa91d
Jimin Ha <jimin9038@g.skku.edu>
2022-03-29 15:36:03 +0000

    shadow round button fix, rename ProblemSet into ProblemSetGroup

--- frontend/src/pages/oj/components/ColorRoundButton.vue
+++ frontend/src/pages/oj/components/ColorRoundButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="rounded-full px-5 h-14 shadow" :style="'background-color:' + color">
+  <button class="color-round-button rounded-full px-5 h-14 hover:shadow-lg transition duration-200 ease-in-out" :style="cssColor">
     <slot></slot>
   </button>
 </template>
@@ -22,11 +22,24 @@ export default {
   async mounted () {
   },
   methods: {
+  },
+  computed: {
+    cssColor () {
+      return {
+        // for opacity purpose. Use tailwindcss v3 class after migrating to tailwindcss v3
+        '--color-opacity': this.color + 'AA',
+        '--color': this.color
+      }
+    }
   }
-
 }
 </script>
 
 <style lang="scss" scoped>
-
+  .color-round-button {
+    background-color: var(--color-opacity);
+  }
+  .color-round-button:hover {
+    background-color: var(--color);
+  }
 </style>
--- frontend/src/pages/oj/components/ProblemSetGroup.vue
+++ frontend/src/pages/oj/components/ProblemSetGroup.vue
@@ -3,13 +3,15 @@
     <div v-for="problemSet of problemSets" :key="problemSet.id">
       <color-round-button
         v-if="problemSetGroup.button_type === 'color-round-button'"
-        :color="problemSet.color" class="mr-5"
+        :color="problemSet.color" class="mr-5 my-3"
+        @click="goProblemSet(problemSet.id)"
       >
         {{ problemSet.title }}
       </color-round-button>
       <shadow-round-button
         v-else-if="problemSetGroup.button_type === 'shadow-round-button'"
-        :color="problemSet.color" class="mr-12"
+        :color="problemSet.color" class="mr-5 my-3"
+        @click="goProblemSet(problemSet.id)"
       >
         {{ problemSet.title }}
       </shadow-round-button>
@@ -23,7 +25,7 @@ import ShadowRoundButton from './ShadowRoundButton.vue'
 import api from '@oj/api'
 
 export default {
-  name: 'ProblemSet',
+  name: 'ProblemSetGroup',
   props: {
     problemSetGroup: {
       type: Object
@@ -45,6 +47,12 @@ export default {
     async getProblemSets () {
       const res = await api.getProblemSet(this.problemSetGroup.id)
       this.problemSets = res.data.data
+    },
+    goProblemSet (problemSetId) {
+      this.$router.push({
+        name: 'problem-set',
+        params: { problemSetId }
+      })
     }
   }
 }
